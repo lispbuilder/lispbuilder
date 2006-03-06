4cf2be50f18ea0c27b385e33c564b86f7bacac7d
Hairytroll <Hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2006-03-06 22:38:47 +0000

    Deleted 'setup.lisp' as running the examples is taken case of by the lispbuilder-sdl-gfx-examples package.

--- lispbuilder-sdl-gfx/setup.lisp
+++
@@ -1,7 +0,0 @@
-;;;; couple of things to do before you run
-
-(asdf:operate 'asdf:load-op :cffi)
-(asdf:operate 'asdf:load-source-op :lispbuilder-sdl)
-(asdf:operate 'asdf:load-source-op :lispbuilder-sdl-gfx)
-
-
