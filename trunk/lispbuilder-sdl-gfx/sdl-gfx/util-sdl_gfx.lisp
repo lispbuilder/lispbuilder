336ba8e9d6babb650ff48bc41ce4f8906ed3880e
Hairytroll <Hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2006-03-08 02:01:06 +0000

    Updated (sdl-gfx:load-sdl-gfx-library) to attempt to load the SDL_gfx.dll binary from the asdf-installed package.

--- trunk/lispbuilder-sdl-gfx/sdl-gfx/util-sdl_gfx.lisp
+++ trunk/lispbuilder-sdl-gfx/sdl-gfx/util-sdl_gfx.lisp
@@ -5,7 +5,7 @@
 ;; This file contains some useful functions for using SDL_gfx from Common lisp
 ;; using sdl_gfx.lisp (the CFFI wrapper)
 
-(in-package :lispbuilder-sdl-gfx) 
+(in-package :lispbuilder-sdl-gfx)
   
 (defparameter *SDL-GFX-LOADED* nil)
 
@@ -13,16 +13,25 @@
 ; Justin TODO Frank had some ideas on how to make this more flexible
 (defparameter *sdl-gfx-dll-filepath* "C:\\SDL_gfx-2.0.13\\lib\\SDL_gfx")
 
+(defparameter *default-sdl-gfx-binaries-path* "SDL_gfx")
 
 ; sdl library and sdl init helpers
 (defun load-sdl-gfx-library()
   "load the sdl library"
   (if *SDL-GFX-LOADED*
       (format t "SDL_gfx runtime already loaded~%")
-      (progn 
-	(format t "Loading SDL_gfx runtime~%")
-	(cffi:load-foreign-library *sdl-gfx-dll-filepath*)
-	(setf *SDL-GFX-LOADED* t))))
+      (progn
+	(let ((path-to-binary nil))
+	  (dolist (path asdf:*central-registry*)
+	    (if (probe-file (merge-pathnames (concatenate 'string *default-sdl-gfx-binaries-path* ".dll")
+					     (eval path)))
+		(setf path-to-binary (merge-pathnames *default-sdl-gfx-binaries-path* (eval path)))))
+	  (if path-to-binary
+	      (format t "SDL_gfx.dll found in location \"~A\"~%" path-to-binary)
+	      (setf path-to-binary *sdl-gfx-dll-filepath*))
+	  (format t "Loading SDL_gfx runtime~%")
+	  (cffi:load-foreign-library path-to-binary)
+	  (setf *SDL-GFX-LOADED* t)))))
 
 (defun unload-sdl-gfx-library()
   "Unload the library when done"
