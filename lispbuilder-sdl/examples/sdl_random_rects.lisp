8bc766edf720253bf4d2cbc5b805d606ed5c891f
Hairytroll <Hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2006-03-24 23:01:18 +0000

    Bug fix: Added (sdl:sdl-load-library) to each example to ensure that the SDL.dll is loaded.

--- lispbuilder-sdl/examples/sdl_random_rects.lisp
+++ lispbuilder-sdl/examples/sdl_random_rects.lisp
@@ -13,6 +13,7 @@
 (in-package :sdl-examples) 
   
 (defun random-rects1 ()
+  (sdl::load-sdl-library)
   (format t "Starting.....~%")
   (let ((width 640) (height 480)
 	(rectangle (sdl::new-rect)))
@@ -35,6 +36,7 @@
 (defun random-rects2 ()
   (let ((width 640) (height 480)
 	(rectangle (sdl::new-rect)))
+    (sdl::load-sdl-library)
     (format t "Starting.....~%")
     (sdl::with-init ()			;Initialize Systems
       (let ((display-surface (sdl::set-window width height)))
@@ -60,6 +62,7 @@
     (cffi:foreign-free rectangle)))
 
 (defun random-rects3 ()
+  (sdl::load-sdl-library)
   (format t "Starting.....~%")
   (let ((width 640) (height 480))
     (sdl::with-init ();Initialize Systems
