925406d01ecb237a923672b545c29c0f0095cf3d
Hairytroll <Hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2006-03-25 17:38:53 +0000

    Fixed a bug where the binary packages were redefining the source packages and blowing away the :nicknames

--- trunk/lispbuilder-opengl/examples/package.lisp
+++ trunk/lispbuilder-opengl/examples/package.lisp
@@ -3,7 +3,7 @@
 (in-package #:cl-user)
 
 (defpackage #:lispbuilder-opengl-examples
-  (:nicknames :gl-examples)
   (:use #:cl #:cffi)
+  (:nicknames #:gl-examples)
   (:documentation "Examples for `lispbuilder-opengl'.")
   (:export #:opengl-gears))
--- trunk/lispbuilder-sdl/examples/package.lisp
+++ trunk/lispbuilder-sdl/examples/package.lisp
@@ -3,8 +3,8 @@
 (in-package #:cl-user)
 
 (defpackage #:lispbuilder-sdl-examples
-  (:nicknames :sdl-examples)
   (:use #:cl #:cffi)
+  (:nicknames #:sdl-examples)
   (:documentation "Examples for `lispbuilder-sdl'.")
   (:export #:bmp_sample #:pixels #:recursive-rects #:mouse-painter #:random-rects1
 	   #:random-rects2 #:random-rects3))
