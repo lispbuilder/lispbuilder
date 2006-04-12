c69c7df9e8ac1549a1974f32ecbda279010b3e30
Hairytroll <Hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2006-04-12 00:33:38 +0000

    John Connors submitted a patch to library.lisp to support SBCL in Linux.

--- trunk/lispbuilder-opengl/opengl/library.lisp
+++ trunk/lispbuilder-opengl/opengl/library.lisp
@@ -3,9 +3,9 @@
 (in-package #:lispbuilder-opengl)
 
 (cffi:define-foreign-library GL
-  (:darwin (:framework "OpenGL"))
-  (:windows "OPENGL32.dll")
-  (:unix (:or "libGL")))
+   (:darwin (:framework "OpenGL"))
+   (:windows "OPENGL32.dll")
+   (:unix (:or "libGL" "libGL.so.2" "libGL.so.1")))
 
 (cffi:use-foreign-library GL)
 
