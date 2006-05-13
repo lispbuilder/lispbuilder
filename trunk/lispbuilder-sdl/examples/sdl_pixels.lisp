3e425314ac6f3154b44f0d1559cd0a39775edf8e
Hairytroll <Hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2006-05-13 03:15:41 +0000

    - Updated examples to reflect new package exports.

--- trunk/lispbuilder-sdl/examples/sdl_pixels.lisp
+++ trunk/lispbuilder-sdl/examples/sdl_pixels.lisp
@@ -6,7 +6,7 @@
 ;;;; To run this sample
 ;;;; (load "setup") which just loads the cffi and lispbuilder-sdl packages
 ;;;; (load "sdl_pixels.lisp")
-;;;; (lispbuilder-sdl::pixels) 
+;;;; (lispbuilder-sdl:pixels) 
 
 (in-package #:sdl-examples) 
   
@@ -19,14 +19,14 @@
 ; utilities used in this sample
 
 (defun random-color(surface)
-  (sdl::SDL_MapRGB (cffi:foreign-slot-value surface 'sdl::SDL_Surface 'sdl::format)
+  (sdl:SDL_MapRGB (cffi:foreign-slot-value surface 'sdl:SDL_Surface 'sdl:format)
 		   (random 256)
 		   (random 256)
 		   (random 256)))
 
 
 (defun pixel-test(surface)
-  (sdl::put-pixel surface (random *SCREEN-WIDTH*) (random *SCREEN-HEIGHT*) (random-color surface)))
+  (sdl:put-pixel surface (random *SCREEN-WIDTH*) (random *SCREEN-HEIGHT*) (random-color surface)))
 
 (defparameter *break-in* nil)
 
@@ -34,26 +34,26 @@
 
 (defun pixels()
   "demonstrates put-pixel and get-pixel as well as color and locking functions"
-  (if (= 0 (sdl::sdl_init sdl::SDL_INIT_VIDEO))
-      (let ((*display-surface* (sdl::SDL_SetVideoMode *SCREEN-WIDTH* *SCREEN-HEIGHT* 0 sdl::SDL_ANYFORMAT)))
+  (if (= 0 (sdl:sdl_init sdl:SDL_INIT_VIDEO))
+      (let ((*display-surface* (sdl:SDL_SetVideoMode *SCREEN-WIDTH* *SCREEN-HEIGHT* 0 sdl:SDL_ANYFORMAT)))
 	(unless (cffi:null-pointer-p *display-surface*)
 	  (format t "SDL_MapRGB test-color in screen format: ~a~%"
-		  (sdl::SDL_MapRGB (cffi:foreign-slot-value *display-surface* 'sdl::SDL_Surface 'sdl::format) 255 0 0))
-	  (with-foreign-object (event_ptr 'sdl::SDL_Event)
+		  (sdl:SDL_MapRGB (cffi:foreign-slot-value *display-surface* 'sdl:SDL_Surface 'sdl:format) 255 0 0))
+	  (with-foreign-object (event_ptr 'sdl:SDL_Event)
 	    (do
 	     ((event-type 0))
-	     ((eql event-type sdl::SDL_QUIT))
-	      (if (sdl::SDL_PollEvent event_ptr)
-		  (setf event-type (cffi:foreign-slot-value event_ptr 'sdl::SDL_Event 'sdl::type)))
-	      (sdl::with-surface-lock *display-surface*
-		(sdl::put-pixel *display-surface* 10 10 (sdl::SDL_MapRGB (cffi:foreign-slot-value *display-surface* 'sdl::SDL_Surface 'sdl::format) 255 0 0))
+	     ((eql event-type sdl:SDL_QUIT))
+	      (if (sdl:SDL_PollEvent event_ptr)
+		  (setf event-type (cffi:foreign-slot-value event_ptr 'sdl:SDL_Event 'sdl:type)))
+	      (sdl:with-surface-lock *display-surface*
+		(sdl:put-pixel *display-surface* 10 10 (sdl:SDL_MapRGB (cffi:foreign-slot-value *display-surface* 'sdl:SDL_Surface 'sdl:format) 255 0 0))
 		(pixel-test *display-surface*))
-	      (sdl::SDL_UpdateRect *display-surface* 0 0 0 0)
-	      (sdl::SDL_Flip *display-surface*)))
+	      (sdl:SDL_UpdateRect *display-surface* 0 0 0 0)
+	      (sdl:SDL_Flip *display-surface*)))
 	  (format t "get-pixel test. pixel at 10 10: ~a~%"
-		  (sdl::get-pixel *display-surface* 10 10))
-	  (sdl::SDL_FreeSurface *display-surface*)
+		  (sdl:get-pixel *display-surface* 10 10))
+	  (sdl:SDL_FreeSurface *display-surface*)
 	  (setf *display-surface* nil)
-	  (sdl::SDL_Quit)))
+	  (sdl:SDL_Quit)))
       (error "Unable to start SDL~%")))
 
--- trunk/lispbuilder-sdl/examples/sdl_simplefont.lisp
+++ trunk/lispbuilder-sdl/examples/sdl_simplefont.lisp
@@ -28,13 +28,13 @@
 ;;;; Should this be in util?
 (defun load-image(filename &optional r g b)
   "load an image and sets transparency if r,g,b are non-null"
-  (let ((loaded-image (sdl::load-bmp filename)))
+  (let ((loaded-image (sdl:load-bmp filename)))
     (when loaded-image
-      (let ((optimized-image (sdl::SDL_DisplayFormat loaded-image)))
-        (sdl::SDL_FreeSurface loaded-image)
+      (let ((optimized-image (sdl:SDL_DisplayFormat loaded-image)))
+        (sdl:SDL_FreeSurface loaded-image)
 	(when optimized-image
 	  (if (and r g b)
-	      (sdl::set-colorkey optimized-image r g b))
+	      (sdl:set-colorkey optimized-image r g b))
 	  optimized-image)))))
 
 (defun initialise-font(bmp-file-name font-width font-height char-map-string key-red key-green key-blue)
@@ -53,32 +53,32 @@
 (defun close-font(font)
   "free up the font image surface"
   (if (font-bmp font)
-      (sdl::SDL_FreeSurface (font-bmp font))))
+      (sdl:SDL_FreeSurface (font-bmp font))))
 
 (defun draw-text-image(screen text-image x y)
   "draw a prerendered string of text"
-  (let ((w (sdl::surf-w text-image))
-	(h (sdl::surf-h text-image)))
-    (sdl::apply-surface-free text-image
+  (let ((w (sdl:surf-w text-image))
+	(h (sdl:surf-h text-image)))
+    (sdl:apply-surface-free text-image
 			     screen
-			     :source-rect (sdl::rectangle 0 0 w h)
+			     :source-rect (sdl:rectangle 0 0 w h)
 			     :destination-x x :destination-y y)))
 
 (defun make-text-image(font string)
   "given an initialised font and a string, draw the string to a surface and return the surface pointer"
   (let ((surface-width (* (font-w font) (length string)))
 	(surface-height (font-h font)))
-    (let ((surface (sdl::create-surface (sdl::sdl_getvideosurface)  
+    (let ((surface (sdl:create-surface (sdl:sdl_getvideosurface)  
 					surface-width surface-height
 					(font-key-r font) (font-key-g font) (font-key-b font))))
-      (sdl::fill-surface surface (font-key-r font) (font-key-g font) (font-key-b font))
+      (sdl:fill-surface surface (font-key-r font) (font-key-g font) (font-key-b font))
       (draw-string surface font 0 0 string)
       surface)))
 
 (defun free-text-image(image)
   "free the sdl surface a text image allocated"
   (if image
-      (sdl::SDL_FreeSurface image)))  
+      (sdl:SDL_FreeSurface image)))  
 
 (defun draw-character(screen font x y char)
   "draw a single character at the x y position"
@@ -87,9 +87,9 @@
 	  (h (font-h font))
 	  (char-offset (get-char-offset font char)))
       (if char-offset
-	  (sdl::apply-surface-free image
+	  (sdl:apply-surface-free image
 				   screen
-				   :source-rect (sdl::rectangle (* w char-offset) 0 w h) 
+				   :source-rect (sdl:rectangle (* w char-offset) 0 w h) 
 				   :destination-x x :destination-y y)
 	nil))))
 
