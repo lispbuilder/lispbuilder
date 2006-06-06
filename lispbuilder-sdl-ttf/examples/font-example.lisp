a360f901fc4e1aeb3b59d0ad550b8567743170b1
Hairytroll <Hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2006-06-06 02:10:05 +0000

    * package.lisp, util-sdl_ttf.lisp   - Added various draw-text-* utils to draw text to a surface.
    * font-example.lisp
      - Updated the example to use the draw-text-solid util.

--- lispbuilder-sdl-ttf/examples/font-example.lisp
+++ lispbuilder-sdl-ttf/examples/font-example.lisp
@@ -14,8 +14,7 @@
 	(sdl-ttf:with-init
 	  (let ((font (sdl-ttf:open-font (namestring *sdl-ttf*) 32)))
 	    (unwind-protect
-		 (let ((text-surf (sdl-ttf:make-text-surface font "Hello TTF World!" #(255 255 255))))
-		   (sdl:blit-surface text-surf display))
+		 (sdl-ttf:draw-text-solid display font "Hello TTF World!" #(255 255 255) nil)
 	      (sdl-ttf:close-font font))))
 	(sdl:with-events
 	  (:quit t)
