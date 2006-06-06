5114b1e04c444706ad99fcbda7495a90df6a10f8
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2006-06-06 09:04:29 +0000

    - Removed example. - Minor updates.

---
+++ lispbuilder-sdl-image/examples/image-example.lisp
@@ -0,0 +1,7 @@
+;;;; Demonstration/Test of using the SDL_image library
+;;;; using CFFI for foreign function interfacing...
+;;;; (C)2006 Luke Crook
+;;;; see COPYING for license
+
+(in-package #:sdl-ttf-examples)
+
