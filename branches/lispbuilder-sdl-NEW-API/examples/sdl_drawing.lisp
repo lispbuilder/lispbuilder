549322a22a44d973ae332fd8f79c6853410b2dd4
Hairytroll <Hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2006-06-26 17:27:23 +0000

    * sdl_drawing.lisp   - Updated Frank's example to take advantage of the new functions in util-sdl.lisp.     Reduced the number of lines considerably.

--- branches/lispbuilder-sdl-NEW-API/examples/sdl_drawing.lisp
+++ branches/lispbuilder-sdl-NEW-API/examples/sdl_drawing.lisp
@@ -11,43 +11,14 @@
 
 (in-package #:sdl-examples)
 
-(defconstant +screen-width+ 640)
-(defconstant +screen-height+ 480)
-
-(defmacro event-loop (event-function event &body events)
-  (let ((rtype (gensym))
-	(idle))
-    `(with-foreign-object (,event 'sdl:SDL_Event)
-      (loop do
-       (if (= 1 (,event-function event))
-	   (let ((,rtype (cffi:foreign-slot-value event 'sdl:SDL_Event
-						  'sdl:type)))
-	     (cond
-	       ,@(loop for (type command) in events
-		       if (eql type :idle) do (setf idle command)
-		       else collect `((= ,rtype ,type) ,command))))
-	   ,(when idle `(progn ,idle)))))))
-
-(defun on-mouse-motion (surface event)
-  (let ((state (cffi:foreign-slot-value event 'sdl:SDL_MouseMotionEvent 'sdl:state))
-	(x (cffi:foreign-slot-value event 'sdl:SDL_MouseMotionEvent 'sdl:x))
-	(y (cffi:foreign-slot-value event 'sdl:SDL_MouseMotionEvent 'sdl:y))
-	(dx (cffi:foreign-slot-value event 'sdl:SDL_MouseMotionEvent 'sdl:xrel))
-	(dy (cffi:foreign-slot-value event 'sdl:SDL_MouseMotionEvent 'sdl:yrel)))
-    (when (= 1 state)
-      (sdl:draw-line x y (- x dx) (- y dy) :surface surface :color #(0 0 0))
-      (sdl:update-screen surface))))
-
 (defun mouse-painter ()
-  (unless (zerop (sdl:sdl_init sdl:SDL_INIT_VIDEO)) (error "Unable to start SDL"))
-  (sdl:SDL_WM_SetCaption "Mouse Painter" "Mouse Painter")
-  (let ((surface (sdl:SDL_SetVideoMode +screen-width+ +screen-height+ 0
-				       sdl:SDL_ANYFORMAT)))
-    (sdl:draw-rect (vector 0 0 +screen-width+ +screen-height+) :surface surface :color #(255 255 255))
-    (event-loop sdl:SDL_WaitEvent event
-		(sdl:SDL_QUIT (loop-finish))
-		(sdl:SDL_MOUSEMOTION (on-mouse-motion surface event))
-		(sdl:SDL_VIDEOEXPOSE (sdl:update-screen surface))
-		(:idle #| use this and SDL_PollEvent if you want to animate something
-		 |#)))
- (sdl:SDL_Quit))
+  (sdl:with-init ()
+    (sdl:with-display (640 480 :title-caption "Mouse Painter" :icon-caption "Mouse Painter")
+      (sdl:clear-screen #(255 255 255))
+      (sdl:with-events
+	(:quit t)
+	(:videoexpose (sdl:update-screen))
+	(:mousemotion (state x y xrel yrel)
+		      (when (= 1 state)
+			(sdl:draw-line x y (- x xrel) (- y yrel) :color #(0 0 0))
+			(sdl:update-screen)))))))
