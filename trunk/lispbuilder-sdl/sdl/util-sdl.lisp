61d3fb5309b4efb4d731152958f9306e858175c2
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2006-09-11 17:48:31 +0000

    * util-sdl   - Added functions for setting the global *DEFAULT-COLOR*, *DEFAULT-POSITION* etc variables.

--- trunk/lispbuilder-sdl/sdl/util-sdl.lisp
+++ trunk/lispbuilder-sdl/sdl/util-sdl.lisp
@@ -15,6 +15,31 @@
 (defvar *default-position* #(0 0))
 (defvar *default-rectangle* #(0 0 0 0))
 
+(defun default-surface ()
+  *default-surface*)
+(defun (setf default-surface) (surface)
+  (setf *default-surface* surface))
+
+(defun default-display ()
+  *default-display*)
+(defun (setf default-display) (surface)
+  (setf *default-display* surface))
+
+(defun default-color ()
+  *default-color*)
+(defun (setf default-color) (color)
+  (setf *default-color* color))
+
+(defun default-position ()
+  *default-position*)
+(defun (setf default-position) (position)
+  (setf *default-position* position))
+
+(defun default-rectangle ()
+  *default-rectangle*)
+(defun (setf default-rectangle) (rectangle)
+  (setf *default-rectangle* rectangle))
+
 ;;;; Macros
 
 ;;; c
