a39ac91f423423e6c1ed82a254bb14fbf5975cbe
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2006-09-22 22:13:45 +0000

    * random_circles.lisp  Average fps is now over a number of frames.

--- trunk/lispbuilder-sdl-gfx/examples/random_circles.lisp
+++ trunk/lispbuilder-sdl-gfx/examples/random_circles.lisp
@@ -6,19 +6,39 @@
 (in-package #:sdl-gfx-examples) 
 
 (defvar *circles-per-frame* 1)
-(defvar *fps-average-window* 500)
 
-(let ((fps-ticks 0) (fps-frame-count 0) (fps-text ""))
+(let* ((frame-values 600)
+       (frame-times (make-array frame-values :initial-element 0 :element-type 'fixnum))
+       (frame-time-last 0)
+       (frame-count 0))
+  (declare (type fixnum frame-values frame-time-last frame-count))
+
+  (defun fps-init (number-of-frames)
+    (setf frame-values number-of-frames
+	  frame-count 0
+	  frame-time-last (sdl:SDL_getticks))
+    (dotimes (i frame-values)
+      (setf (aref frame-times i) 0)))
+
   (defun display-fps (surface)
-    (sdl:draw-rect :rectangle #(5 65 150 17) :color #(0 0 0) :surface surface)   
-    (incf fps-frame-count)
-    (when (> (incf fps-ticks (sdl::get-ticks)) *fps-average-window*)
-      (setf fps-text (format nil "Current FPS : ~d" (sdl:to-int (coerce (/ fps-frame-count
-									   (/ fps-ticks 1000))
-									'float))))
-      (setf fps-frame-count 0
-	    fps-ticks 0))
-    (sdl-gfx:draw-string fps-text :position #(10 70) :surface surface :color #(255 255 255))))
+    (declare (optimize (safety 0) (speed 3) (space 1)))
+    (let ((get-ticks (sdl:SDL_getticks))
+          (frames-per-second 0.0))
+      (declare (type fixnum get-ticks)
+	       (type float frames-per-second))
+
+      (setf (aref frame-times frame-count) (- get-ticks frame-time-last))
+      (setf frame-time-last get-ticks)
+      (incf frame-count)
+      (when (>= frame-count frame-values)
+	(setf frame-count 0)
+	(dotimes (i frame-values)
+	  (incf frames-per-second (aref frame-times i)))
+	(setf frames-per-second (the float (/ 1000 (/ frames-per-second frame-values))))
+	(sdl:fill-surface :color #(0 0 0) :surface surface :update-p t)
+	(sdl-gfx:draw-string (format nil "fps : ~d" (coerce frames-per-second 'float))
+			     :position #(0 0) :surface surface :color #(255 255 255)))
+      surface)))
 
 (defun random-circles ()
   (sdl:with-init ()
@@ -27,6 +47,8 @@
       (let ((width (sdl:surf-w sdl:*default-display*))
 	    (height (sdl:surf-h sdl:*default-display*)))
 	(sdl:set-framerate 0)
+	(fps-init)
+	(sdl:with-surfaces-free ((fps (sdl:create-surface 150 8 :surface sdl:*default-display* :accel t)))
 	(sdl:with-events ()
 	  (:quit () t)
 	  (:keydown (:key key)
@@ -37,5 +59,5 @@
 	     (sdl-gfx:draw-filledCircle (random 100)
 					:position (sdl:point (random width) (random height))
 					:color (sdl:color (random 255) (random 255) (random 255) (random 255))))
-	   (display-fps sdl:*default-surface*)
-	   (sdl:update-display)))))))
+	   (sdl:blit-surface :src (display-fps fps) :dst sdl:*default-display* :dst-rect #(10 260) :free-p nil)
+	   (sdl:update-display))))))))
