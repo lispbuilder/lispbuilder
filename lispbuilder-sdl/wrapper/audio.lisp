765b4173df718f7f87721ae5586cdedb56e89c76
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2006-12-08 18:16:57 +0000

    * Renamed everything in CamelCase to camel-case * Placed the CFFI definitions for each *.h file into the corresponding *.lisp file.

---
+++ lispbuilder-sdl/wrapper/audio.lisp
@@ -0,0 +1,116 @@
+
+(in-package #:lispbuilder-sdl-base)
+
+
+(cffi:defcstruct sdl-Audio-Spec
+	(freq :int)
+	(format :unsigned-short)
+	(channels :unsigned-char)
+	(silence :unsigned-char)
+	(samples :unsigned-short)
+	(padding :unsigned-short)
+	(size :unsigned-int)
+	(callback :pointer)
+	(userdata :pointer))
+
+(cl:defconstant AUDIO-U8 #x0008)
+
+(cl:defconstant AUDIO-S8 #x8008)
+
+(cl:defconstant AUDIO-U16LSB #x0010)
+
+(cl:defconstant AUDIO-S16LSB #x8010)
+
+(cl:defconstant AUDIO-U16MSB #x1010)
+
+(cl:defconstant AUDIO-S16MSB #x9010)
+
+(cl:defconstant AUDIO-U16 #x0010)
+
+(cl:defconstant AUDIO-S16 #x8010)
+
+(cffi:defcstruct Sdl-Audio-Cvt
+	(needed :int)
+	(src-format :unsigned-short)
+	(dst-format :unsigned-short)
+	(rate-incr :double)
+	(buf :pointer)
+	(len :int)
+	(len-cvt :int)
+	(len-mult :int)
+	(len-ratio :double)
+	(filters :pointer)
+	(filter-index :int))
+
+(cffi:defcfun ("SDL_AudioInit" SDL-Audio-Init) :int
+  (driver-name :string))
+
+(cffi:defcfun ("SDL_AudioQuit" SDL-Audio-Quit) :void)
+
+(cffi:defcfun ("SDL_AudioDriverName" SDL-Audio-Driver-Name) :string
+  (namebuf :string)
+  (maxlen :int))
+
+(cffi:defcfun ("SDL_OpenAudio" SDL-Open-Audio) :int
+  (desired :pointer)
+  (obtained :pointer))
+
+(cffi:defcenum SDL-audio-status
+	(:SDL-AUDIO-STOPPED 0)
+	:SDL-AUDIO-PLAYING
+	:SDL-AUDIO-PAUSED)
+
+(cffi:defcfun ("SDL_GetAudioStatus" SDL-Get-Audio-Status) SDL-audio-status)
+
+(cffi:defcfun ("SDL_PauseAudio" SDL-Pause-Audio) :void
+  (pause-on :int))
+
+(cffi:defcfun ("SDL_LoadWAV_RW" SDL-Load-WAV-RW) :pointer
+  (src :pointer)
+  (freesrc :int)
+  (spec :pointer)
+  (audio-buf :pointer)
+  (audio-len :pointer))
+
+(cffi:defcfun ("SDL_FreeWAV" SDL-Free-WAV) :void
+  (audio-buf :pointer))
+
+(cffi:defcfun ("SDL_BuildAudioCVT" SDL-Build-Audio-CVT) :int
+  (cvt :pointer)
+  (src-format :unsigned-short)
+  (src-channels :unsigned-char)
+  (src-rate :int)
+  (dst-format :unsigned-short)
+  (dst-channels :unsigned-char)
+  (dst-rate :int))
+
+(cffi:defcfun ("SDL_ConvertAudio" SDL-Convert-Audio) :int
+  (cvt :pointer))
+
+(cl:defconstant SDL-MIX-MAXVOLUME 128)
+
+(cffi:defcfun ("SDL_MixAudio" SDL-Mix-Audio) :void
+  (dst :pointer)
+  (src :pointer)
+  (len :unsigned-int)
+  (volume :int))
+
+(cffi:defcfun ("SDL_LockAudio" SDL-Lock-Audio) :void)
+
+(cffi:defcfun ("SDL_UnlockAudio" SDL-Unlock-Audio) :void)
+
+(cffi:defcfun ("SDL_CloseAudio" SDL-Close-Audio) :void)
+
+
+(defun SDL-Load-WAV (file spec audio-buf audio-len)
+  (SDL-Load-WAV-RW (SDL-RW-FROM-FILE file "rb")
+		  1
+		  spec
+		  audio-buf
+		  audio-len))
+
+;;;; Must define the CPU byte order.
+#-(or little-endian PC386 X86 I386) (defconstant AUDIO-U16SYS AUDIO-U16MSB) ;; Big Endian
+#-(or little-endian PC386 X86 I386) (defconstant AUDIO-S16SYS AUDIO-S16MSB) ;; Big Endian
+#+(or little-endian PC386 X86 I386) (defconstant AUDIO-U16SYS AUDIO-U16LSB) ;; Little Endian
+#+(or little-endian PC386 X86 I386) (defconstant AUDIO-S16SYS AUDIO-S16LSB) ;; Little Endian
---
+++ lispbuilder-sdl/wrapper/version.lisp
@@ -0,0 +1,39 @@
+
+(in-package #:lispbuilder-sdl-base) 
+
+;;;; Implementation of SDL macros follows
+
+(cl:defconstant SDL-MAJOR-VERSION 1)
+
+(cl:defconstant SDL-MINOR-VERSION 2)
+
+(cl:defconstant SDL-PATCH-LEVEL 11)
+
+(cffi:defcstruct SDL-version
+	(major :unsigned-char)
+	(minor :unsigned-char)
+	(patch :unsigned-char))
+
+(cffi:defcfun ("SDL_Linked_Version" SDL-Linked-Version) :pointer)
+
+
+
+(defun SDL-VERSION (x)
+  (cffi:with-foreign-slots ((major minor patch) x SDL-version)
+    (setf major SDL-MAJOR-VERSION
+          minor SDL-MINOR-VERSION
+          patch SDL-PATCH-LEVEL)))
+
+(defun SDL-VERSION-NUM (major minor patch)
+        (+  (* major 1000)
+            (* minor 100)
+            patch))
+
+(defun SDL-COMPILED-VERSION ()
+        (SDL-VERSION-NUM SDL-MAJOR-VERSION SDL-MINOR-VERSION SDL-PATCH-LEVEL))
+
+(defun SDL-VERSION-AT-LEAST (x y z)
+  (if (>= (SDL-COMPILED-VERSION) (SDL-VERSION-NUM x y z))
+      1
+      0))
+
