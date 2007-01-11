168ddda85879b4676f707867077b6a08637ebdd6
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2007-01-11 22:12:41 +0000

    * Moved the generic function PACK-COLOR into lispbuilder-sdl.

--- trunk/lispbuilder-sdl-gfx/sdl-gfx/sdl-gfx-util.lisp
+++ trunk/lispbuilder-sdl-gfx/sdl-gfx/sdl-gfx-util.lisp
@@ -75,170 +75,122 @@
 	do (draw-pixel point)))))
 
 (defun draw-pixel (position &key (surface sdl:*default-surface*) (color sdl:*default-color*))
-  (if (typep color 'sdl:color)
-      (sdl-gfx-cffi::pixel-color (sdl:fp surface) (sdl:x position) (sdl:y position)
-				 (map-color color))
-      (sdl-gfx-cffi::pixel-rgba (sdl:fp surface) (sdl:x position) (sdl:y position)
-				(sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
+  (draw-pixel-* (sdl:x position) (sdl:y position) :surface surface :color color))
 
 (defun draw-pixel-* (x y &key (surface sdl:*default-surface*) (color sdl:*default-color*))
   (if (typep color 'sdl:color)
       (sdl-gfx-cffi::pixel-color (sdl:fp surface) x y
-				 (map-color color))
+				 (sdl:pack-color color))
       (sdl-gfx-cffi::pixel-rgba (sdl:fp surface) x y
 				(sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
 
 (defun draw-hline (x1 x2 y &key (surface sdl:*default-surface*) (color sdl:*default-color*))
   (if (typep color 'sdl:color)
       (sdl-gfx-cffi::hline-color (sdl:fp surface) x1 x2 y
-				 (map-color color))
+				 (sdl:pack-color color))
       (sdl-gfx-cffi::hline-RGBA (sdl:fp surface) x1 x2 y
 				(sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
 
 (defun draw-vline (x y1 y2 &key (surface sdl:*default-surface*) (color sdl:*default-color*))
   (if (typep color 'sdl:color)
       (sdl-gfx-cffi::vline-color (sdl:fp surface) x y1 y2
-				 (map-color color))
+				 (sdl:pack-color color))
       (sdl-gfx-cffi::vline-RGBA (sdl:fp surface) x y1 y2
 				(sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
 
 (defun draw-rectangle (rect &key (surface sdl:*default-surface*) (color sdl:*default-color*))
-  (if (typep color 'sdl:color)
-      (sdl-gfx-cffi::rectangle-color (sdl:fp surface) (sdl:x rect) (sdl:y rect) (sdl:x2 rect) (sdl:y2 rect)
-				     (map-color color))
-      (sdl-gfx-cffi::rectangle-RGBA (sdl:fp surface) (sdl:x rect) (sdl:y rect) (sdl:x2 rect) (sdl:y2 rect)
-				    (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
+  (draw-rectangle-* (sdl:x rect) (sdl:y rect) (sdl:width rect) (sdl:height rect) :surface surface :color color))
 
 (defun draw-rectangle-* (x y w h &key (surface sdl:*default-surface*) (color sdl:*default-color*))
-  (if (typep color 'sdl:color)
-      (sdl-gfx-cffi::rectangle-color (sdl:fp surface) x y (+ x w) (+ y h)
-				     (map-color color))
-      (sdl-gfx-cffi::rectangle-RGBA (sdl:fp surface) x y (+ x w) (+ y h)
-				    (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
+  (draw-rectangle-edges-* x y (+ x w) (+ y h) :surface surface :color color))
 
 (defun draw-rectangle-edges-* (x1 y1 x2 y2 &key (surface sdl:*default-surface*) (color sdl:*default-color*))
   (if (typep color 'sdl:color)
       (sdl-gfx-cffi::rectangle-color (sdl:fp surface) x1 y1 x2 y2
-				     (map-color color))
+				     (sdl:pack-color color))
       (sdl-gfx-cffi::rectangle-RGBA (sdl:fp surface) x1 y1 x2 y2
 				    (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
 
 (defun draw-box (rect &key (surface sdl:*default-surface*) (color sdl:*default-color*))
-  (if (typep color 'sdl:color)
-      (sdl-gfx-cffi::box-color (sdl:fp surface) (sdl:x rect) (sdl:y rect) (sdl:x2 rect) (sdl:y2 rect)
-			       (map-color color))
-      (sdl-gfx-cffi::box-RGBA (sdl:fp surface) (sdl:x rect) (sdl:y rect) (sdl:x2 rect) (sdl:y2 rect)
-			      (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
+  (draw-box-* (sdl:x rect) (sdl:y rect) (sdl:width rect) (sdl:height rect) :surface surface :color color))
 
 (defun draw-box-* (x y w h &key (surface sdl:*default-surface*) (color sdl:*default-color*))
-  (if (typep color 'sdl:color)
-      (sdl-gfx-cffi::box-color (sdl:fp surface) x y (+ x w) (+ y h)
-			       (map-color color))
-      (sdl-gfx-cffi::box-RGBA (sdl:fp surface) x y (+ x w) (+ y h)
-			      (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
+  (draw-box-edges-* x y (+ x w) (+ y h) :surface surface :color color))
 
 (defun draw-box-edges-* (x1 y1 x2 y2 &key (surface sdl:*default-surface*) (color sdl:*default-color*))
   (if (typep color 'sdl:color)
       (sdl-gfx-cffi::box-color (sdl:fp surface) x1 y1 x2 y2
-			       (map-color color))
+			       (sdl:pack-color color))
       (sdl-gfx-cffi::box-RGBA (sdl:fp surface) x1 y1 x2 y2
 			      (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
 
 (defun draw-line (point1 point2 &key (surface sdl:*default-surface*) (color sdl:*default-color*))
-  (if (typep color 'sdl:color)
-      (sdl-gfx-cffi::line-color (sdl:fp surface) (sdl:x point1) (sdl:y point1) (sdl:x point2) (sdl:y point2)
-				(map-color color))
-      (sdl-gfx-cffi::line-RGBA (sdl:fp surface) (sdl:x point1) (sdl:y point1) (sdl:x point2) (sdl:y point2)
-			       (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
+  (draw-line-* (sdl:x point1) (sdl:y point1) (sdl:x point2) (sdl:y point2) :surface surface :color color))
 
 (defun draw-line-* (x1 y1 x2 y2 &key (surface sdl:*default-surface*) (color sdl:*default-color*))
   (if (typep color 'sdl:color)
       (sdl-gfx-cffi::line-color (sdl:fp surface) x1 y1 x2 y2
-				(map-color color))
+				(sdl:pack-color color))
       (sdl-gfx-cffi::line-RGBA (sdl:fp surface) x1 y1 x2 y2
 			       (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
 
 (defun draw-aa-line (point1 point2 &key (surface sdl:*default-surface*) (color sdl:*default-color*))
-  (if (typep color 'sdl:color)
-      (sdl-gfx-cffi::aa-line-color (sdl:fp surface) (sdl:x point1) (sdl:y point1) (sdl:x point2) (sdl:y point2)
-				   (map-color color))
-      (sdl-gfx-cffi::aa-line-RGBA (sdl:fp surface) (sdl:x point1) (sdl:y point1) (sdl:x point2) (sdl:y point2)
-				  (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
+  (draw-aa-line-* (sdl:x point1) (sdl:y point1) (sdl:x point2) (sdl:y point2) :surface surface :color color))
 
 (defun draw-aa-line-* (x1 y1 x2 y2 &key (surface sdl:*default-surface*) (color sdl:*default-color*))
   (if (typep color 'sdl:color)
       (sdl-gfx-cffi::aa-line-color (sdl:fp surface) x1 y1 x2 y2
-				   (map-color color))
+				   (sdl:pack-color color))
       (sdl-gfx-cffi::aa-line-RGBA (sdl:fp surface) x1 y1 x2 y2
 				  (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
 
 (defun draw-circle (p1 r &key (surface sdl:*default-surface*) (color sdl:*default-color*))
-  (if (typep color 'sdl:color)
-      (sdl-gfx-cffi::circle-color (sdl:fp surface) (sdl:x p1) (sdl:y p1) r
-				  (map-color color))
-      (sdl-gfx-cffi::circle-RGBA (sdl:fp surface) (sdl:x p1) (sdl:y p1) r
-				 (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
+  (draw-circle-* (sdl:x p1) (sdl:y p1) r :surface surface :color color))
 
 (defun draw-circle-* (x y r &key (surface sdl:*default-surface*) (color sdl:*default-color*))
   (if (typep color 'sdl:color)
       (sdl-gfx-cffi::circle-color (sdl:fp surface) x y r
-				  (map-color color))
+				  (sdl:pack-color color))
       (sdl-gfx-cffi::circle-RGBA (sdl:fp surface) x y r
 				 (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
 
 (defun draw-aa-circle (p1 r &key (surface sdl:*default-surface*) (color sdl:*default-color*))
-  (if (typep color 'sdl:color)
-      (sdl-gfx-cffi::aa-circle-color (sdl:fp surface) (sdl:x p1) (sdl:y p1) r
-				     (map-color color))
-      (sdl-gfx-cffi::aa-circle-RGBA (sdl:fp surface) (sdl:x p1) (sdl:y p1) r
-				    (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
+  (draw-aa-circle-* (sdl:x p1) (sdl:y p1) r :surface surface :color color))
 
 (defun draw-aa-circle-* (x y r &key (surface sdl:*default-surface*) (color sdl:*default-color*))
   (if (typep color 'sdl:color)
       (sdl-gfx-cffi::aa-circle-color (sdl:fp surface) x y r
-				     (map-color color))
+				     (sdl:pack-color color))
       (sdl-gfx-cffi::aa-circle-RGBA (sdl:fp surface) x y r
 				    (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
 
 (defun draw-filled-circle (p1 r &key (surface sdl:*default-surface*) (color sdl:*default-color*))
-  (if (typep color 'sdl:color)
-      (sdl-gfx-cffi::filled-circle-color (sdl:fp surface) (sdl:x p1) (sdl:y p1) r
-					 (map-color color))
-      (sdl-gfx-cffi::filled-circle-RGBA (sdl:fp surface) (sdl:x p1) (sdl:y p1) r
-					(sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
+  (draw-filled-circle-* (sdl:x p1) (sdl:y p1) r :surface surface :color color))
 
 (defun draw-filled-circle-* (x y r &key (surface sdl:*default-surface*) (color sdl:*default-color*))
   (if (typep color 'sdl:color)
       (sdl-gfx-cffi::filled-circle-color (sdl:fp surface) x y r
-					 (map-color color))
+					 (sdl:pack-color color))
       (sdl-gfx-cffi::filled-circle-RGBA (sdl:fp surface) x y r
 					(sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
 
 (defun draw-ellipse (p1 rx ry &key (surface sdl:*default-surface*) (color sdl:*default-color*))
-  (if (typep color 'sdl:color)
-      (sdl-gfx-cffi::ellipse-color (sdl:fp surface) (sdl:x p1) (sdl:y p1) rx ry
-				   (map-color color))
-      (sdl-gfx-cffi::ellipse-RGBA (sdl:fp surface) (sdl:x p1) (sdl:y p1) rx ry
-				  (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
+  (draw-ellipse-* (sdl:x p1) (sdl:y p1) rx ry :surface surface :color color))
 
 (defun draw-ellipse-* (x y rx ry &key (surface sdl:*default-surface*) (color sdl:*default-color*))
   (if (typep color 'sdl:color)
       (sdl-gfx-cffi::ellipse-color (sdl:fp surface) x y rx ry
-				   (map-color color))
+				   (sdl:pack-color color))
       (sdl-gfx-cffi::ellipse-RGBA (sdl:fp surface) x y rx ry
 				  (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
 
 (defun draw-aa-ellipse (p1 rx ry &key (surface sdl:*default-surface*) (color sdl:*default-color*))
-  (if (typep color 'sdl:color)
-      (sdl-gfx-cffi::aa-ellipse-color (sdl:fp surface) (sdl:x p1) (sdl:y p1) rx ry
-				      (map-color color))
-      (sdl-gfx-cffi::aa-ellipse-RGBA (sdl:fp surface) (sdl:x p1) (sdl:y p1) rx ry
-				     (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
+  (draw-aa-ellipse-* (sdl:x p1) (sdl:y p1) rx ry :surface surface :color color))
 
 (defun draw-aa-ellipse-* (x y rx ry &key (surface sdl:*default-surface*) (color sdl:*default-color*))
   (if (typep color 'sdl:color)
       (sdl-gfx-cffi::aa-ellipse-color (sdl:fp surface) x y rx ry
-				      (map-color color))
+				      (sdl:pack-color color))
       (sdl-gfx-cffi::aa-ellipse-RGBA (sdl:fp surface) x y rx ry
 				     (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
 
@@ -250,35 +202,27 @@
     (error "ERROR, draw-filled-ellipse-*: Parameters must be of type :short."))
   (if (typep color 'sdl:color)
       (sdl-gfx-cffi::filled-ellipse-color (sdl:fp surface) x y rx ry
-					  (map-color color))
+					  (sdl:pack-color color))
       (sdl-gfx-cffi::filled-ellipse-RGBA (sdl:fp surface) x y rx ry
 					 (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
 
 (defun draw-pie (p1 rad start end &key (surface sdl:*default-surface*) (color sdl:*default-color*))
-  (if (typep color 'sdl:color)
-      (sdl-gfx-cffi::pie-color (sdl:fp surface) (sdl:x p1) (sdl:y p1) rad start end
-			       (map-color color))
-      (sdl-gfx-cffi::pie-RGBA (sdl:fp surface) (sdl:x p1) (sdl:y p1) rad start end
-			      (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
+  (draw-pie-* (sdl:x p1) (sdl:y p1) rad start end :surface surface :color color))
 
 (defun draw-pie-* (x y rad start end &key (surface sdl:*default-surface*) (color sdl:*default-color*))
   (if (typep color 'sdl:color)
       (sdl-gfx-cffi::pie-color (sdl:fp surface) x y rad start end
-			       (map-color color))
+			       (sdl:pack-color color))
       (sdl-gfx-cffi::pie-RGBA (sdl:fp surface) x y rad start end
 			      (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
 
 (defun draw-filled-pie (p1 rad start end &key (surface sdl:*default-surface*) (color sdl:*default-color*))
-  (if (typep color 'sdl:color)
-      (sdl-gfx-cffi::filled-pie-color (sdl:fp surface) (sdl:x p1) (sdl:y p1) rad start end
-				      (map-color color))
-      (sdl-gfx-cffi::filled-pie-RGBA (sdl:fp surface) (sdl:x p1) (sdl:y p1) rad start end
-				     (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
+  (draw-filled-pie-* (sdl:x p1) (sdl:y p1) rad start end :surface surface :color color))
 
 (defun draw-filled-pie-* (x y rad start end &key (surface sdl:*default-surface*) (color sdl:*default-color*))
   (if (typep color 'sdl:color)
       (sdl-gfx-cffi::filled-pie-color (sdl:fp surface) x y rad start end
-				      (map-color color))
+				      (sdl:pack-color color))
       (sdl-gfx-cffi::filled-pie-RGBA (sdl:fp surface) x y rad start end
 				     (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
 
@@ -287,7 +231,7 @@
       (sdl-gfx-cffi::trigon-color (sdl:fp surface) (sdl:x point1) (sdl:y point1)
 				  (sdl:x point2) (sdl:y point2)
 				  (sdl:x point3) (sdl:y point3)
-				  (map-color color))
+				  (sdl:pack-color color))
       (sdl-gfx-cffi::trigon-RGBA (sdl:fp surface) (sdl:x point1) (sdl:y point1)
 				 (sdl:x point2) (sdl:y point2)
 				 (sdl:x point3) (sdl:y point3)
@@ -298,7 +242,7 @@
       (sdl-gfx-cffi::aa-trigon-color (sdl:fp surface) (sdl:x point1) (sdl:y point1)
 				     (sdl:x point2) (sdl:y point2)
 				     (sdl:x point3) (sdl:y point3)
-				     (map-color color))
+				     (sdl:pack-color color))
       (sdl-gfx-cffi::aa-trigon-RGBA (sdl:fp surface) (sdl:x point1) (sdl:y point1)
 				    (sdl:x point2) (sdl:y point2)
 				    (sdl:x point3) (sdl:y point3)
@@ -309,7 +253,7 @@
       (sdl-gfx-cffi::filled-trigon-color (sdl:fp surface) (sdl:x point1) (sdl:y point1)
 					 (sdl:x point2) (sdl:y point2)
 					 (sdl:x point3) (sdl:y point3)
-					 (map-color color))
+					 (sdl:pack-color color))
       (sdl-gfx-cffi::filled-trigon-RGBA (sdl:fp surface) (sdl:x point1) (sdl:y point1)
 					(sdl:x point2) (sdl:y point2)
 					(sdl:x point3) (sdl:y point3)
@@ -323,7 +267,7 @@
 	(poly-surface nil))
     (if (typep color 'sdl:color)
 	(setf poly-surface (sdl-gfx-cffi::polygon-color (sdl:fp surface) x-array y-array (length points)
-							(map-color color)))
+							(sdl:pack-color color)))
 	(setf poly-surface (sdl-gfx-cffi::polygon-RGBA (sdl:fp surface) x-array y-array (length points)
 						       (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
     (cffi:foreign-free x-array)
@@ -338,7 +282,7 @@
 	(poly-surface nil))
     (if (typep color 'sdl:color)
 	(setf poly-surface (sdl-gfx-cffi::aa-polygon-color (sdl:fp surface) x-array y-array (length points)
-							   (map-color color)))
+							   (sdl:pack-color color)))
 	(setf poly-surface (sdl-gfx-cffi::aa-polygon-RGBA (sdl:fp surface) x-array y-array (length points)
 							  (sdl:r color) (sdl:g color) (sdl:b color)
 							  (sdl:a color))))
@@ -354,7 +298,7 @@
 	(poly-surface nil))
     (if (typep color 'sdl:color)
 	(setf poly-surface (sdl-gfx-cffi::filled-polygon-color (sdl:fp surface) x-array y-array (length points)
-							       (map-color color)))
+							       (sdl:pack-color color)))
 	(setf poly-surface (sdl-gfx-cffi::filled-polygon-RGBA (sdl:fp surface) x-array y-array (length points)
 							      (sdl:r color) (sdl:g color) (sdl:b color)
 							      (sdl:a color))))
@@ -369,7 +313,7 @@
 	(y-array (cffi:foreign-alloc :short :initial-contents (return-list-for-array points :y)))
 	(bezier-surface nil))
     (if (typep color 'sdl:color)
-	(setf bezier-surface (sdl-gfx-cffi::bezier-color (sdl:fp surface) x-array y-array (length points) steps (map-color color)))
+	(setf bezier-surface (sdl-gfx-cffi::bezier-color (sdl:fp surface) x-array y-array (length points) steps (sdl:pack-color color)))
 	(setf bezier-surface (sdl-gfx-cffi::bezier-RGBA (sdl:fp surface) x-array y-array (length points) steps
 							(sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
     (cffi:foreign-free x-array)
@@ -377,50 +321,24 @@
     bezier-surface))
 
 (defun draw-character (c p1 &key (surface sdl:*default-surface*) (color sdl:*default-color*))
-  (if (typep color 'sdl:color)
-      (sdl-gfx-cffi::character-color (sdl:fp surface) (sdl:x p1) (sdl:y p1) c
-				     (map-color color))
-      (sdl-gfx-cffi::character-RGBA (sdl:fp surface) (sdl:x p1) (sdl:y p1) c
-				    (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
+  (draw-character-* c (sdl:x p1) (sdl:y p1) :surface surface :color color))
 
 (defun draw-character-* (c x y &key (surface sdl:*default-surface*) (color sdl:*default-color*))
   (if (typep color 'sdl:color)
       (sdl-gfx-cffi::character-color (sdl:fp surface) x y c
-				     (map-color color))
+				     (sdl:pack-color color))
       (sdl-gfx-cffi::character-RGBA (sdl:fp surface) x y c
 				    (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
 
 (defun draw-string (c p1 &key (surface sdl:*default-surface*) (color sdl:*default-color*))
-  (if (typep color 'sdl:color)
-      (sdl-gfx-cffi::string-color (sdl:fp surface) (sdl:x p1) (sdl:y p1) c
-				  (map-color color))
-      (sdl-gfx-cffi::string-RGBA (sdl:fp surface) (sdl:x p1) (sdl:y p1) c
-				 (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
+  (draw-string-* c (sdl:x p1) (sdl:y p1) :surface surface :color color))
 
 (defun draw-string-* (c x y &key (surface sdl:*default-surface*) (color sdl:*default-color*))
   (if (typep color 'sdl:color)
       (sdl-gfx-cffi::string-color (sdl:fp surface) x y c
-				  (map-color color))
+				  (sdl:pack-color color))
       (sdl-gfx-cffi::string-RGBA (sdl:fp surface) x y c
 				 (sdl:r color) (sdl:g color) (sdl:b color) (sdl:a color))))
-
-;;; m
-
-(defmethod map-color ((color sdl:color))
-  (let ((col #x00000000))
-    (setf col (logior (ash (sdl:r color) 24)
-		      (ash (sdl:g color) 16)
-		      (ash (sdl:b color) 8)
-		      #xFF))
-    col))
-
-(defmethod map-color ((color sdl:color-a))
-  (let ((col #x00000000))
-    (setf col (logior (ash (sdl:r color) 24)
-		      (ash (sdl:g color) 16)
-		      (ash (sdl:b color) 8)
-		      (sdl:a color)))
-    col))
 
 ;;; r
 
