40130cd390b1395d8823f68630ddb7aa04287331
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2007-01-24 17:28:07 +0000

    * Added support for 'Ubuntu Edgy' thanks to Anthony Fairchild   - Added "libSDL-1.2.so.0" to CFFI:DEFINE-FOREIGN-LIBRARY

--- lispbuilder-sdl/cffi/library.lisp
+++ lispbuilder-sdl/cffi/library.lisp
@@ -10,6 +10,10 @@
 (cffi:define-foreign-library sdl
   (:darwin (:framework "SDL"))
   (:windows "SDL.dll")
-  (:unix (:or "libSDL" "libSDL.so" "libSDL-1.2.so" "libSDL-1.2.so.0.7.2")))
+  (:unix (:or "libSDL"
+	      "libSDL.so"
+	      "libSDL-1.2.so"
+	      "libSDL-1.2.so.0"
+	      "libSDL-1.2.so.0.7.2")))
 
 (cffi:use-foreign-library sdl)
