90424e1dd911e05ce16431938201a82539c14c17
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2007-02-06 03:44:59 +0000

    * Fixed the example to actually use UTF8 encoding when it said so.

--- trunk/lispbuilder-sdl-ttf/examples/font-example.lisp
+++ trunk/lispbuilder-sdl-ttf/examples/font-example.lisp
@@ -11,23 +11,20 @@
     (sdl-ttf:initialise-default-font)
     (sdl:with-surface (disp sdl:*default-display*)
       (sdl-ttf:draw-string-solid-* "Text LATIN1 - Solid" 0 0
-				   :color (sdl:color :r 255 :g 0 :b 255)
-				   :encoding :latin1)
+				   :color (sdl:color :r 255 :g 0 :b 255))
       (sdl-ttf:draw-string-solid-* "Text UTF8 - Solid" 0 50
 				   :color (sdl:color :r 255 :g 0 :b 255)
 				   :encoding :utf8)
       (sdl-ttf:draw-string-shaded-* "Text LATIN1 - Shaded" 0 100
-				    (sdl:color :r 255 :g 0 :b 255) (sdl:color :r 255 :g 255 :b 255)
-				   :encoding :latin1)
+				    (sdl:color :r 255 :g 0 :b 255) (sdl:color :r 255 :g 255 :b 255))
       (sdl-ttf:draw-string-shaded-* "Text UTF8 - Shaded" 0 150
 				    (sdl:color :r 255 :g 0 :b 255) (sdl:color :r 255 :g 255 :b 255)
-				    :encoding :latin1)
+				    :encoding :utf8)
       (sdl-ttf:draw-string-blended-* "Text LATIN1 - Blended" 0 200
-				     :color (sdl:color :r 255 :g 0 :b 255)
-				     :encoding :latin1)
+				     :color (sdl:color :r 255 :g 0 :b 255))
       (sdl-ttf:draw-string-blended-* "Text UTF8 - Blended" 0 250
 				     :color (sdl:color :r 255 :g 0 :b 255)
-				     :encoding :latin1))
+				     :encoding :utf8))
     (sdl:with-events ()
       (:quit-event () t)
       (:video-expose-event () (sdl:update-display)))))
