d33af26cb576f4a4de050926e42923f021673fa2
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2007-02-07 17:50:48 +0000

    * RENDER-TEXT-SOLID, RENDER-TEXT-BLENDED, RENDER-TEXT-SHADED   - Modified so that the cached surface is freed when :FREE T.

--- trunk/lispbuilder-sdl-ttf/sdl-ttf/string-solid.lisp
+++ trunk/lispbuilder-sdl-ttf/sdl-ttf/string-solid.lisp
@@ -7,8 +7,10 @@
 (defun render-string-solid (text &key
 			    (encoding :latin1)
 			    (font *default-font*)
-			    (color sdl:*default-color*))
+			    (color sdl:*default-color*)
+			    (free nil))
   "Render text TEXT using font FONT with color COLOR into the FONT's cached surface, using the Solid mode. 
+Unless :FREE T, the caller is responsible for freeing the new SURFACE.
 
   * TEXT is the text to render. TEXT may be of the encoding type LATIN1, UTF8, UNICODE, GLYPH. TEXT must match :ENCODING
 
@@ -22,7 +24,9 @@
 
   * COLOR color is the color used to render text, of type SDL:SDL-COLOR
 
-  * Returns the cached SDL:SDL-SURFACE.
+  * FREE when T will free the old cached SURFACE in FONT.
+
+  * Returns the new cached surface SDL:SDL-SURFACE.
 
 For example:
   * (DRAW-STRING-SOLID \"Hello World!\" :ENCODING :LATIN1 :FONT *DEFAULT-FONT* :COLOR A-COLOR)"
@@ -30,7 +34,7 @@ For example:
     (error "ERROR: RENDER-STRING-SOLID; FONT must be of type FONT."))
   (unless (typep color 'sdl:sdl-color)
     (error "ERROR: RENDER-STRING-SOLID; COLOR must be of type SDL:SDL-COLOR."))
-  (when (sdl:cached-surface font)
+  (when (and free (sdl:cached-surface font))
     (sdl:free-surface (sdl:cached-surface font)))
   (case encoding
     (:latin1
@@ -71,7 +75,8 @@ For example:
 			    (surface sdl:*default-surface*)
 			    (color sdl:*default-color*))
   "Draw text TEXT using font FONT with color COLOR onto surface SURFACE, using the Solid mode. 
-Caches the new surface in the FONT object.
+Caches the new SURFACE containing the TEXT in the FONT object. 
+Note: Calls RENDER-STRING-SOLID with :FREE T so the old cached surface is automatically free'd. 
 
   * TEXT is the text to render. TEXT may be of the encoding type LATIN1, UTF8, UNICODE, GLYPH. TEXT must match :ENCODING
 
@@ -89,13 +94,13 @@ Caches the new surface in the FONT object.
 
   * COLOR color is the color used to render text, of type SDL:SDL-COLOR
 
-  * Returns the cached SDL:SDL-SURFACE.
+  * Returns the font FONT.
 
 For example:
   * (DRAW-STRING-SOLID-* \"Hello World!\" 0 0 :ENCODING :LATIN1 :FONT *DEFAULT-FONT* :SURFACE A-SURFACE :COLOR A-COLOR)"
   (unless (typep surface 'sdl:sdl-surface)
     (error "ERROR: DRAW-STRING-SOLID-*; SURFACE must be of type SDL:SDL-SURFACE."))
-  (let ((font-surface (render-string-solid text :encoding encoding :font font :color color)))
+  (let ((font-surface (render-string-solid text :encoding encoding :font font :color color :free t)))
     (sdl:set-surface-* font-surface :x x :y y)
     (sdl:blit-surface font-surface surface)
-    font-surface))
+    font))
