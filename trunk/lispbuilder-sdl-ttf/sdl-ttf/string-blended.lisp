2603cd1ffe87d77b33bb1153ec913821faafc247
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2007-02-10 02:05:19 +0000

    * Support for UTF8 only.

--- trunk/lispbuilder-sdl-ttf/sdl-ttf/string-blended.lisp
+++ trunk/lispbuilder-sdl-ttf/sdl-ttf/string-blended.lisp
@@ -6,7 +6,6 @@
 
 
 (defun render-string-blended (text &key
-			      (encoding :latin1)
 			      (font *default-font*)
 			      (color sdl:*default-color*)
 			      (free nil)
@@ -15,13 +14,7 @@
 Unless :FREE T, the caller is responsible for freeing the new SURFACE.
 Use :CACHE T to cache the newly created surface in the FONT object.
 
-  * TEXT is the text to render. TEXT may be of the encoding type LATIN1, UTF8, UNICODE, GLYPH. TEXT must match :ENCODING
-
-  * ENCODING specifies the format of the text to render and is one of: 
-    * :LATIN1
-    * :UTF8
-    * :UNICODE
-    * :GLYPH
+  * TEXT is the text to render. 
 
   * FONT is a FONT object.  Bound to *DEFAULT-FONT* by default. 
 
@@ -34,67 +27,42 @@ Use :CACHE T to cache the newly created surface in the FONT object.
   * Returns the new cached surface SDL:SDL-SURFACE.
 
 For example:
-  * (DRAW-STRING-BLENDED \"Hello World!\" :ENCODING :LATIN1 :FONT *DEFAULT-FONT* :COLOR A-COLOR)"
-  (unless (typep font 'font)
-    (error "ERROR: RENDER-STRING-BLENDED; FONT must be of type FONT."))
-  (unless (typep color 'sdl:sdl-color)
-    (error "ERROR: RENDER-STRING-BLENDED; COLOR must be of type SDL:SDL-COLOR."))
+  * (DRAW-STRING-BLENDED \"Hello World!\" :FONT *DEFAULT-FONT* :COLOR A-COLOR)"
+  (check-type font font "FONT must be of type FONT.")
+  (check-type color sdl:sdl-color "COLOR must be of type SDL:SDL-COLOR.")
   (when (and free (sdl:cached-surface font))
     (sdl:free-surface (sdl:cached-surface font))
     (setf (sdl:cached-surface font) nil))
   (let ((surf 
-	 (case encoding
-	   (:latin1
-	    (sdl:with-foreign-color-copy (col-struct color)
-	      (sdl:surface (sdl-ttf-cffi::ttf-Render-Text-blended (fp-font font) text col-struct))))
-	   (:UTF8
-	    (sdl:with-foreign-color-copy (col-struct color)
-	      (sdl:surface (sdl-ttf-cffi::ttf-Render-UTF8-blended (fp-font font) text col-struct))))
-	   (:GLYPH
-	    (sdl:with-foreign-color-copy (col-struct color)
-	      (sdl:surface (sdl-ttf-cffi::ttf-Render-Glyph-blended (fp-font font) text col-struct))))
-	   (:UNICODE
-	    ;; TODO
-	    )
-	   (otherwise
-	    (error "ERROR: RENDER-STRING-BLENDED; ENCODING must be one of :LATIN1, :UTF8, :GLYPH or :UNICODE.")))))
+	 (sdl:with-foreign-color-copy (col-struct color)
+	   (sdl:surface (sdl-ttf-cffi::ttf-Render-UTF8-blended (fp-font font) text col-struct)))))
     (when cache
       (setf (sdl:cached-surface font) surf))
     surf))
 
 
 (defun draw-string-blended (text position &key
-			    (encoding :latin1)
 			    (font *default-font*)
 			    (surface sdl:*default-surface*)
 			    (color sdl:*default-color*))
   "See DRAW-STRING-BLENDED-*.
   * :POSITION is the x and y position to render the text, of type SDL:POINT."
-  (unless (typep position 'sdl:point)
-    (error "ERROR: DRAW-STRING-BLENDED; POSITION must be of type SDL:POINT."))
+  (check-type position sdl:point "POSITION must be of type SDL:POINT")
   (draw-string-blended-* text (sdl:x position) (sdl:y position)
-			 :encoding encoding
 			 :font font
 			 :surface surface
 			 :color color))
 
 
 (defun draw-string-blended-* (text x y &key
-			      (encoding :latin1)
 			      (font *default-font*)
 			      (surface sdl:*default-surface*)
 			      (color sdl:*default-color*))
   "Draw text TEXT using font :FONT with color :COLOR onto surface :SURFACE, using the Blended mode. 
 
-  * TEXT is the text to render. TEXT may be of the encoding type LATIN1, UTF8, UNICODE, GLYPH. TEXT must match :ENCODING
+  * TEXT is the text to render. 
 
   * X/Y are the x and y position coordinates, as INTEGERS.
-
-  * ENCODING specifies the format of the text to render and is one of: 
-    * :LATIN1
-    * :UTF8
-    * :UNICODE
-    * :GLYPH
 
   * FONT is a FONT object.  Bound to *DEFAULT-FONT* by default. 
 
@@ -105,11 +73,9 @@ For example:
   * Returns the font FONT.
 
 For example:
-  * (DRAW-STRING-SOLID-* \"Hello World!\" 0 0 :ENCODING :LATIN1 :FONT *DEFAULT-FONT* :SURFACE A-SURFACE :COLOR A-COLOR)"
-  (unless (typep surface 'sdl:sdl-surface)
-    (error "ERROR: DRAW-STRING-BLENDED-*; SURFACE must be of type SDL:SDL-SURFACE."))
+  * (DRAW-STRING-SOLID-* \"Hello World!\" 0 0 :FONT *DEFAULT-FONT* :SURFACE A-SURFACE :COLOR A-COLOR)"
+  (check-type surface sdl:sdl-surface "SURFACE must be of type SDL:SDL-SURFACE.")
   (sdl:with-surface (font-surface (render-string-blended text
-							 :encoding encoding
 							 :font font
 							 :color color
 							 :cache nil
--- trunk/lispbuilder-sdl-ttf/sdl-ttf/string-solid.lisp
+++ trunk/lispbuilder-sdl-ttf/sdl-ttf/string-solid.lisp
@@ -5,7 +5,6 @@
 (in-package #:lispbuilder-sdl-ttf)
 
 (defun render-string-solid (text &key
-			    (encoding :latin1)
 			    (font *default-font*)
 			    (color sdl:*default-color*)
 			    (free nil)
@@ -14,13 +13,7 @@
 Unless :FREE T, the caller is responsible for freeing the new SURFACE.
 Use :CACHE T to cache the newly created surface in the FONT object.
 
-  * TEXT is the text to render. TEXT may be of the encoding type LATIN1, UTF8, UNICODE, GLYPH. TEXT must match :ENCODING
-
-  * ENCODING specifies the format of the text to render and is one of: 
-    * :LATIN1
-    * :UTF8
-    * :UNICODE
-    * :GLYPH
+  * TEXT is the text to render.
 
   * FONT is a FONT object.  Bound to *DEFAULT-FONT* by default. 
 
@@ -33,67 +26,41 @@ Use :CACHE T to cache the newly created surface in the FONT object.
   * Returns the new cached surface SDL:SDL-SURFACE.
 
 For example:
-  * (DRAW-STRING-SOLID \"Hello World!\" :ENCODING :LATIN1 :FONT *DEFAULT-FONT* :COLOR A-COLOR)"
-  (unless (typep font 'font)
-    (error "ERROR: RENDER-STRING-SOLID; FONT must be of type FONT."))
-  (unless (typep color 'sdl:sdl-color)
-    (error "ERROR: RENDER-STRING-SOLID; COLOR must be of type SDL:SDL-COLOR."))
+  * (DRAW-STRING-SOLID \"Hello World!\" :FONT *DEFAULT-FONT* :COLOR A-COLOR)"
+  (check-type font font "FONT must be of type FONT.")
+  (check-type color sdl:sdl-color "COLOR must be of type SDL:SDL-COLOR.")
   (when (and free (sdl:cached-surface font))
     (sdl:free-surface (sdl:cached-surface font))
     (setf (sdl:cached-surface font) nil))
   (let ((surf
-	 (case encoding
-	   (:latin1
-	    (sdl:with-foreign-color-copy (col-struct color)
-	      (sdl:surface (sdl-ttf-cffi::ttf-Render-Text-Solid (fp-font font) text col-struct))))
-	   (:UTF8
-	    (sdl:with-foreign-color-copy (col-struct color)
-	      (sdl:surface (sdl-ttf-cffi::ttf-Render-UTF8-Solid (fp-font font) text col-struct))))
-	   (:GLYPH
-	    (sdl:with-foreign-color-copy (col-struct color)
-	      (sdl:surface (sdl-ttf-cffi::ttf-Render-Glyph-Solid (fp-font font) text col-struct))))
-	   (:UNICODE
-	    ;; TODO
-	    )
-	   (otherwise
-	    (error "ERROR: RENDER-STRING-SOLID; ENCODING must be one of :LATIN1, :UTF8, :GLYPH or :UNICODE.")))))
+	 (sdl:with-foreign-color-copy (col-struct color)
+	   (sdl:surface (sdl-ttf-cffi::ttf-Render-UTF8-Solid (fp-font font) text col-struct)))))
     (when cache
       (setf (sdl:cached-surface font) surf))
-	surf))
-
+    surf))
 
 (defun draw-string-solid (text position &key 
-			  (encoding :latin1)
 			  (font *default-font*)
 			  (surface sdl:*default-surface*)
 			  (color sdl:*default-color*))
   "See DRAW-STRING-SOLID-*.
 
   * POSITION is the x and y position to render the text, of type SDL:POINT."
-  (unless (typep surface 'sdl:point)
-    (error "ERROR: DRAW-STRING-SOLID; POSITION must be of type SDL:POINT."))
+  (check-type position sdl:point "POSITION must be of type SDL:POINT") 
   (draw-string-solid-* text (sdl:x position) (sdl:y position)
-		       :encoding encoding
 		       :font font
 		       :surface surface
 		       :color color))
 
 (defun draw-string-solid-* (text x y &key 
-			    (encoding :latin1)
 			    (font *default-font*)
 			    (surface sdl:*default-surface*)
 			    (color sdl:*default-color*))
   "Draw text TEXT using font FONT with color COLOR onto surface SURFACE, using the Solid mode. 
 
-  * TEXT is the text to render. TEXT may be of the encoding type LATIN1, UTF8, UNICODE, GLYPH. TEXT must match :ENCODING
+  * TEXT is the text to render.
 
   * X/Y are the x and y position coordinates, as INTEGERS.
-
-  * ENCODING specifies the format of the text to render and is one of: 
-    * :LATIN1
-    * :UTF8
-    * :UNICODE
-    * :GLYPH
 
   * FONT is a FONT object.  Bound to *DEFAULT-FONT* by default. 
 
@@ -104,11 +71,9 @@ For example:
   * Returns the font FONT.
 
 For example:
-  * (DRAW-STRING-SOLID-* \"Hello World!\" 0 0 :ENCODING :LATIN1 :FONT *DEFAULT-FONT* :SURFACE A-SURFACE :COLOR A-COLOR)"
-  (unless (typep surface 'sdl:sdl-surface)
-    (error "ERROR: DRAW-STRING-SOLID-*; SURFACE must be of type SDL:SDL-SURFACE."))
+  * (DRAW-STRING-SOLID-* \"Hello World!\" 0 0 :FONT *DEFAULT-FONT* :SURFACE A-SURFACE :COLOR A-COLOR)"
+  (check-type surface sdl:sdl-surface "SURFACE must be of type SDL:SDL-SURFACE.")
   (sdl:with-surface (font-surface (render-string-solid text
-						       :encoding encoding
 						       :font font
 						       :color color
 						       :free nil
