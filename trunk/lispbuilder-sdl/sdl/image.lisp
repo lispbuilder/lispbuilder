4b089443fae96bd19151440e1c1d8037904855ef
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2007-03-05 17:36:19 +0000

    * Fixed the examples that load BMP files.

--- trunk/lispbuilder-sdl/sdl/image.lisp
+++ trunk/lispbuilder-sdl/sdl/image.lisp
@@ -2,15 +2,14 @@
 (in-package #:lispbuilder-sdl)
 
 
-(defun load-image (filename path &key
-		   key-color alpha-value)
+(defun load-image (filename &key key-color alpha-value)
   (let ((surf (surface (sdl-base::load-image filename))))
     (if surf
 	(progn
 	  (when key-color (set-color-key key-color :surface surf))
 	  (when alpha-value (set-alpha alpha-value :surface surf))
 	  surf)
-	(error "ERROR, LOAD-IMAGE: file ~A, ~A not found" filename path))))
+	(error "ERROR, LOAD-IMAGE: file ~A, ~A not found" filename))))
 
 (defun save-image (surface filename)
   "save the supplied filename, must be a bmp file"
