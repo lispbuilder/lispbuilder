8f3250c8a867e0a80a51446607bf7cff347cec62
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2007-07-26 21:57:06 +0000

    * Fixed to support SDL_ttf.dll 2.0.9

--- trunk/lispbuilder-sdl-ttf/cffi/library.lisp
+++ trunk/lispbuilder-sdl-ttf/cffi/library.lisp
@@ -7,14 +7,21 @@
 	   cffi:*foreign-library-directories*
 	   :test #'equal))
 
+#+win32(cffi:define-foreign-library zlib1
+	 (:windows "zlib1.dll"))
+#+win32(cffi:define-foreign-library libfreetype-6
+	 (:windows "libfreetype-6.dll"))
+
 (cffi:define-foreign-library sdl-ttf
   (:darwin (:framework "libSDL_ttf-2.0"))
-  (:windows (:or "SDL_ttf.dll" "SDL_ttf2.0.dll"))
+  (:windows (:or "SDL_ttf.dll"))
   (:unix (:or "libSDL_ttf2.0" "libSDL_ttf-2.0.so.0")))
 
 (cffi:define-foreign-library sdl-ttf-glue
   (:windows "lispbuilder-sdl-ttf-glue.dll")
   (:unix "lispbuilder-sdl-ttf-glue.so"))
 
+(cffi:use-foreign-library zlib1)
+(cffi:use-foreign-library libfreetype-6)
 (cffi:use-foreign-library sdl-ttf)
 (cffi:use-foreign-library sdl-ttf-glue)
