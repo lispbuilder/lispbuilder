5a85b51feed25389623832d0a44859296750a41b
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2007-07-30 23:16:31 +0000

    * Fixed the following bugs reported by Michal Dybizbanski   - Forgot to commit utils.lisp to SVN.   - Modified the name of the SDL_gfx library to load for OS X.

---
+++ trunk/lispbuilder-sdl-gfx/examples/utils.lisp
@@ -0,0 +1,16 @@
+
+(in-package #:sdl-gfx-examples)
+
+(defun every-n-frames (max)
+  (let ((count 0))
+    #'(lambda ()
+	(if (eql 0 (mod (incf count 1) max))
+	    (setf count 0)
+	    nil))))
+
+(defun draw-fps (string x y font surface render-p)
+  ;; Create a new FPS string when render-p is T
+  (when render-p
+    (sdl-gfx:render-string-shaded string sdl:*white* sdl:*black* :font font :cache t :free t))
+  ;; Draw the string each frame
+  (sdl-gfx:draw-font-at-* x y :font font :surface surface))
