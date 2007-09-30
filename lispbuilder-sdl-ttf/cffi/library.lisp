5320078140e1986e187635ffe42370a502536913
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2007-09-30 07:37:22 +0000

    - Reapplied the fixes for OS X.

--- lispbuilder-sdl-ttf/cffi/library.lisp
+++ lispbuilder-sdl-ttf/cffi/library.lisp
@@ -9,11 +9,13 @@
 
 (cffi:define-foreign-library zlib1
   (:windows "zlib1.dll")
-  (:unix "libz.so"))
+  ;; (:unix "libz.so")
+  )
 
 (cffi:define-foreign-library libfreetype-6
   (:windows "libfreetype-6.dll")
-  (:unix "libfreetype.so"))
+  ;; (:unix "libfreetype.so")
+  )
 
 (cffi:define-foreign-library sdl-ttf
   (:darwin (:or (:framework "SDL_ttf")
@@ -22,12 +24,16 @@
   (:unix (:or "libSDL_ttf" "libSDL_ttf2.0" "libSDL_ttf-2.0.so.0")))
 
 (cffi:define-foreign-library sdl-ttf-glue
-  (:darwin "lispbuilder-sdl-ttf-glue.dylib")
-  (:windows "lispbuilder-sdl-ttf-glue.dll")
+  (:darwin (:or "liblispbuilder-sdl-ttf-glue.dylib"
+		"lispbuilder-sdl-ttf-glue.dylib"))
+  (:windows (:or "liblispbuilder-sdl-ttf-glue.dll"
+		 "lispbuilder-sdl-ttf-glue.dll"))
   (:unix "liblispbuilder-sdl-ttf-glue.so"))
 
+;; Load the win32 required libraries.
 (cffi:use-foreign-library zlib1)
 (cffi:use-foreign-library libfreetype-6)
+
 ;; Load the general sdl libraries.
 (cffi:use-foreign-library sdl-ttf)
 (cffi:use-foreign-library sdl-ttf-glue)
