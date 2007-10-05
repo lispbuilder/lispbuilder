b6dfd295d64cc79e11f707c1fcf8e930db44abfc
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2007-10-05 07:15:45 +0000

    - Minor Updated.

--- lispbuilder-sdl/glue-vecto/glue-vecto.lisp
+++ lispbuilder-sdl/glue-vecto/glue-vecto.lisp
@@ -1,9 +1,8 @@
 
 ;; SDL (Simple Media Layer) library using CFFI for foreign function interfacing...
 ;; (C)2006 Luke Crook <luke@balooga.com>
-;; Thanks to Frank Buss and Surendra Singh
-;; see COPYING for license
-;; This file contains some useful functions for using SDL from Common lisp
+;; This file contains some useful functions for using Zach Beane's VECTO with lispbuilder-SDL
+;; http://www.xach.com/lisp/vecto/
 
 (in-package #:lispbuilder-sdl)
 
