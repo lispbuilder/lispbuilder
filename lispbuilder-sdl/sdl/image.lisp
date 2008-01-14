26c87a7541897c4da15cab357275cd6e0bfbca17
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2008-01-14 09:33:07 +0000

    * Bugfix in LOAD-IMAGE, and CREATE-SURFACE.... how did this happen?

--- lispbuilder-sdl/sdl/image.lisp
+++ lispbuilder-sdl/sdl/image.lisp
@@ -5,7 +5,7 @@
 (defmethod load-image ((filename string)
 		       &key key-color alpha-value (image-type nil) (force nil) (free nil)
 		       key-color-at)
-  "Load a BMP image from a file at location `FILENAME`.
+  "Load a BMP image from a file at location `FILENAME`. Returns a new SURFACE.
 
 ##### Parameters
 
@@ -20,16 +20,13 @@ is `NIL`, the surface is created without a key color. See
 When `ALPHA` is `NIL`, the new surface is created without alpha transparency. See
 [SET-ALPHA](#set-alpha) for more detailed information."
   (declare (ignore image-type force free))
-;  (format t "load-image~%")
-  (let ((surf (surface (sdl-base::load-image filename))))
-    (if surf
-	(progn
-	  (if key-color-at
-	      (set-color-key (read-pixel key-color-at :surface surf) :surface surf)
-	      (when key-color (set-color-key key-color :surface surf)))
-	  (when alpha-value (set-alpha alpha-value :surface surf))
-	  surf)
-	(error "ERROR, LOAD-IMAGE: file ~A not found" filename))))
+  (let ((surf (make-instance 'surface
+			     :surface (sdl-base::load-image filename)
+			     :key-color key-color
+			     :alpha-value alpha-value)))
+    (when key-color-at
+      (set-color-key (read-pixel key-color-at :surface surf) :surface surf))
+    surf))
 
 (defun save-image (surface filename)
   "Saves the surface `SURFACE` as a BMP image to a file at location `FILENAME`."
