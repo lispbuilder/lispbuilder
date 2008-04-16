6a1bd695f25584638655661839c28921e162faf3
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2008-04-16 01:05:35 +0000

    * Adding documentation strings.

---
+++ trunk/lispbuilder-sdl-mixer/sdl-mixer/globals.lisp
@@ -0,0 +1,9 @@
+
+(in-package #:sdl-mixer)
+
+(defvar *default-music* nil)
+(defvar *default-chunk* nil)
+
+(defvar *channel-finished* nil)
+(defvar *music-finished* nil)
+(defvar *audio-buffer* nil)
