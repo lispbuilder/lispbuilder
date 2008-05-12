0bdf88c59a81de444d60011e086efe2c6a6eb420
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2008-05-12 07:36:09 +0000

    * LOAD-IMAGE now accepts RWOPS, STRING, PATHNAME for the location of the image file.

--- lispbuilder-sdl-image/examples/image-example.lisp
+++ lispbuilder-sdl-image/examples/image-example.lisp
@@ -13,27 +13,27 @@
     (setf (sdl:frame-rate) 5)
     (sdl:initialise-default-font)
     (let ((images (list (sdl:draw-string-solid-* "BMP" 0 0 :color *yellow*
-						 :surface (sdl:load-image (sdl:create-path "lisp.bmp" *bmp-path*)
-									  :key-color (sdl:color :r 253 :g 59 :b 251)))
+						 :surface (sdl-image:load-image (sdl:create-path "lisp.bmp" *bmp-path*)
+										:key-color (sdl:color :r 253 :g 59 :b 251)))
 			(sdl:draw-string-solid-* "GIF" 0 0 :color *yellow*
-						 :surface (sdl:load-image (sdl:create-path "lisp.gif" *bmp-path*)))
+						 :surface (sdl-image:load-image (sdl:create-path "lisp.gif" *bmp-path*)))
 			(sdl:draw-string-solid-* "LBM" 0 0 :color *yellow*
-						 :surface (sdl:load-image (sdl:create-path "lisp.lbm" *bmp-path*)
-									  :key-color (sdl:color :r 253 :g 59 :b 251)))
+						 :surface (sdl-image:load-image (sdl:create-path "lisp.lbm" *bmp-path*)
+										:key-color (sdl:color :r 253 :g 59 :b 251)))
 			(sdl:draw-string-solid-* "PCX" 0 0 :color *yellow*
-						 :surface (sdl:load-image (sdl:create-path "lisp.pcx" *bmp-path*)
-									  :key-color (sdl:color :r 253 :g 59 :b 251)))
+						 :surface (sdl-image:load-image (sdl:create-path "lisp.pcx" *bmp-path*)
+										:key-color (sdl:color :r 253 :g 59 :b 251)))
 			(sdl:draw-string-solid-* "PBM" 0 0 :color *yellow*
-						 :surface (sdl:load-image (sdl:create-path "lisp.pbm" *bmp-path*)))
+						 :surface (sdl-image:load-image (sdl:create-path "lisp.pbm" *bmp-path*)))
 			(sdl:draw-string-solid-* "PPM" 0 0 :color *yellow*
-						 :surface (sdl:load-image (sdl:create-path "lisp.ppm" *bmp-path*)
-									  :key-color (sdl:color :r 253 :g 59 :b 251)))
+						 :surface (sdl-image:load-image (sdl:create-path "lisp.ppm" *bmp-path*)
+										:key-color (sdl:color :r 253 :g 59 :b 251)))
 			(sdl:draw-string-solid-* "PGM" 0 0 :color *yellow*
-						 :surface (sdl:load-image (sdl:create-path "lisp.pgm" *bmp-path*)))
+						 :surface (sdl-image:load-image (sdl:create-path "lisp.pgm" *bmp-path*)))
 			(sdl:draw-string-solid-* "TGA" 0 0 :color *yellow*
-						 :surface (sdl:load-image (sdl:create-path "lisp.tga" *bmp-path*)
-									  :image-type :TGA ; TGA must be specified
-									  :key-color (sdl:color :r 253 :g 59 :b 251))))))
+						 :surface (sdl-image:load-image (sdl:create-path "lisp.tga" *bmp-path*)
+										:image-type :TGA ; TGA must be specified
+										:key-color (sdl:color :r 253 :g 59 :b 251))))))
       (loop
 	 for image in images
 	 for i from 0
--- lispbuilder-sdl-image/sdl-image/package.lisp
+++ lispbuilder-sdl-image/sdl-image/package.lisp
@@ -3,24 +3,24 @@
 (in-package #:cl-user)
 
 ;; sdl-image-util.lisp
-(eval-when (:execute :compile-toplevel :load-toplevel)
-  (mapcar #'(lambda (symbol)
-	      (intern symbol 'lispbuilder-sdl)
-	      (export (find-symbol symbol 'lispbuilder-sdl) 'lispbuilder-sdl))
-	  (list "IMAGE-P"
-		"IMAGE-TYPE-OF"
-		"LOAD-IMAGE"
-		"LOAD-AND-CONVERT-IMAGE")))
+;; (eval-when (:execute :compile-toplevel :load-toplevel)
+;;   (mapcar #'(lambda (symbol)
+;; 	      (intern symbol 'lispbuilder-sdl)
+;; 	      (export (find-symbol symbol 'lispbuilder-sdl) 'lispbuilder-sdl))
+;; 	  (list "IMAGE-P"
+;; 		"IMAGE-TYPE-OF"
+;; 		"LOAD-IMAGE"
+;; 		"LOAD-AND-CONVERT-IMAGE")))
 	  
 (defpackage #:lispbuilder-sdl-image
   (:use #:cl #:cffi)
   (:nicknames #:sdl-image)
   (:documentation "The methods defined here extend any methods already defined in `lispbuilder-sdl'.")
-  (:import-from #:lispbuilder-sdl
-		lispbuilder-sdl:image-p
-		lispbuilder-sdl:image-type-of
-		lispbuilder-sdl:load-and-convert-image
-		lispbuilder-sdl:load-image)
+  ;; (:import-from #:lispbuilder-sdl
+;; 		lispbuilder-sdl:image-p
+;; 		lispbuilder-sdl:image-type-of
+;; 		lispbuilder-sdl:load-and-convert-image
+;; 		lispbuilder-sdl:load-image)
   (:export
    ;; sdl-image-util.lisp
    #:image-p
