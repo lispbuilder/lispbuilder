4180554f409e21f93c15a99f27a23dda2e8d4703
hairytroll <hairytroll@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2008-07-20 08:18:47 +0000

    * Added (LOAD-IMAGE VECTOR) and (LOAD-IMAGE RWOPS)

--- lispbuilder-sdl/sdl/image.lisp
+++ lispbuilder-sdl/sdl/image.lisp
@@ -11,38 +11,24 @@
       (set-color-key (read-pixel key-color-at :surface surf) :surface surf))
     surf))
 
-;; (defmethod load-image ((source sdl:rwops) &key key-color surface-alpha image-type force (free t) (key-color-at nil))
-;;   "Creates and returns a new surface from the image contained in the `RWOPS` structure in the source `SOURCE`.
+(defmethod load-image ((source rwops) &key key-color surface-alpha image-type force free (key-color-at nil))
+  (declare (ignore image-type force))
+  (let ((surf (make-instance 'surface
+			     :surface (sdl-cffi::SDL-Load-BMP-RW (fp source) 0)
+			     :key-color key-color
+			     :surface-alpha surface-alpha)))
+    (when key-color-at
+      (set-color-key (read-pixel key-color-at :surface surf) :surface surf))
+    (when free
+      (sdl:free-rwops source))
+    surf))
 
-;; ##### Parameters
-
-;; * `SOURCE` is of type `RWOPS`.
-;; * `FREE` when `T` will automatically free the `RWOPS` in `SOURCE`."
-;;   (let ((image nil))
-;;     (setf image (if image-type
-;; 		    (if force
-;; 			(case image-type
-;; 			  (:BMP (sdl-image-cffi::img-Load-BMP-RW (sdl:fp source)))
-;; 			  (:GIF (sdl-image-cffi::img-Load-GIF-RW (sdl:fp source)))
-;; 			  (:JPG (sdl-image-cffi::img-Load-JPG-RW (sdl:fp source)))
-;; 			  (:LBM (sdl-image-cffi::img-Load-LBM-RW (sdl:fp source)))
-;; 			  (:PCX (sdl-image-cffi::img-Load-PCX-RW (sdl:fp source)))
-;; 			  (:PNG (sdl-image-cffi::img-Load-PNG-RW (sdl:fp source)))
-;; 			  (:PNM (sdl-image-cffi::img-Load-PNM-RW (sdl:fp source)))
-;; 			  (:TGA (sdl-image-cffi::img-Load-TGA-RW (sdl:fp source)))
-;; 			  (:TIF (sdl-image-cffi::img-Load-TIF-RW (sdl:fp source)))
-;; 			  (:XCF (sdl-image-cffi::img-Load-XCF-RW (sdl:fp source)))
-;; 			  (:XPM (sdl-image-cffi::img-Load-XPM-RW (sdl:fp source)))
-;; 			  (:XV  (sdl-image-cffi::img-Load-XV-RW  (sdl:fp source))))
-;; 			(sdl-image-cffi::img-load-typed-rw (sdl:fp source) nil image-type))
-;; 		    (sdl-image-cffi::img-Load-RW (sdl:fp source) nil)))
-;;     (when free
-;;       (sdl:free-rwops source))
-;;     (when (sdl-base::is-valid-ptr image)
-;;       (let ((surface (make-instance 'sdl:surface :surface image :key-color key-color :surface-alpha surface-alpha)))
-;; 	(when key-color-at
-;; 	  (sdl:set-color-key (sdl:read-pixel key-color-at :surface surface) :surface surface))
-;; 	surface))))
+(defmethod load-image ((source VECTOR) &key key-color surface-alpha image-type force free (key-color-at nil))
+  (declare (ignore image-type force free))
+  (let ((surf (load-image (create-RWops-from-byte-array source)
+			  :key-color key-color :surface-alpha surface-alpha
+			  :free t :key-color-at key-color-at)))
+    surf))
 
 (defun save-image (surface filename)
   "Saves the surface `SURFACE` as a BMP image to a file at location `FILENAME`."
--- lispbuilder-sdl/sdl/rwops.lisp
+++ lispbuilder-sdl/sdl/rwops.lisp
@@ -16,3 +16,49 @@
     (if (sdl-base::is-valid-ptr rwops)
 	(make-instance 'rwops :rwops rwops)
 	nil)))
+
+(defmethod create-RWops-from-byte-array (array)
+  "Creates and returns a new `RWOPS` object from the Lisp array `ARRAY`."
+  (let* ((mem-array (cffi:foreign-alloc :unsigned-char :initial-contents array))
+	 (rwops (make-instance 'rwops :rwops (sdl-cffi::sdl-rw-from-mem mem-array (length array)))))
+    rwops))
+
+(defun file-to-byte-sequence (filepath)
+  "Load a file into an Array of unsigned-byte 8"
+  (with-open-file (str filepath :element-type '(unsigned-byte 8))
+    (let* ((length (file-length str))
+	   (content (make-array (list length)
+				:element-type '(unsigned-byte 8)
+				:adjustable nil)))
+      (read-sequence content str)
+      content)))
+
+(defmethod load-image-from-byte-sequence (array)
+  (let* ((mem-array (cffi:foreign-alloc :unsigned-char :initial-contents array))
+	 (surf (make-instance 'surface :surface (sdl-cffi::sdl-load-bmp-rw (sdl-cffi::sdl-rw-from-mem mem-array (length array)) 1))))
+    surf))
+
+;; (defmethod load-image-from-byte-sequence (array)
+;;   (cffi:with-foreign-object (mem-array :unsigned-char (length array))
+;;     (loop for i from 0 below (length array) do
+;; 	 (setf (cffi:mem-aref mem-array :unsigned-char i) (aref array i)))
+;;     (make-instance 'surface :surface (sdl-cffi::sdl-load-bmp-rw (sdl-cffi::sdl-rw-from-mem mem-array (length array)) 1))))
+
+;; (sdl:with-init ()
+;;   (sdl:window 400 400)
+;;   (sdl:draw-surface (load-image-from-byte-sequence
+;; 		     (file-to-byte-sequence "/home/bin/central-registry/lispbuilder-sdl/examples/lisp.bmp")))
+;;   (sdl:update-display)
+;;   (sdl:with-events ()
+;;     (:quit-event () t)
+;;     (:video-expose-event (sdl:update-display))))
+
+;; (sdl:with-init ()
+;;   (sdl:window 400 400)
+;;   (sdl:draw-surface (load-image (file-to-byte-sequence "/home/bin/central-registry/lispbuilder-sdl/examples/lisp.bmp")
+;; 				:key-color-at #(0 0)))
+;;   (sdl:update-display)
+;;   (sdl:with-events ()
+;;     (:quit-event () t)
+;;     (:video-expose-event (sdl:update-display))))
+
