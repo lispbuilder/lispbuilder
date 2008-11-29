3106dca563d410d4256f620d184358cf8f3ba470
luke@balooga.com <luke@balooga.com@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2008-11-29 09:22:11 +0000

    * Moved the Truetype front from lispbuilder-sdl-ttf to lispbuilder-sdl-ttf/assets. * lispbuilder-sdl-ttf now uses the APIs defined in lispbuilder-sdl .

--- trunk/lispbuilder-sdl-ttf/sdl-ttf/package.lisp
+++ trunk/lispbuilder-sdl-ttf/sdl-ttf/package.lisp
@@ -6,61 +6,21 @@
   (:use #:cl #:cffi)
   (:nicknames #:sdl-ttf)
   (:documentation "The main package of `lispbuilder-sdl-ttf'.")
-  (:import-from #:lispbuilder-sdl
-		lispbuilder-sdl:draw-font
-		lispbuilder-sdl:draw-font-at
-		lispbuilder-sdl:draw-font-at-*)
   (:export
-
+   
    ;; globals.lisp
-   #:*default-font*
-   #:*default-font-path*
    #:*generation*
    
    ;; font.lisp
-   #:font
-   #:new-font
-;;    #:x
-;;    #:y
-;;    #:fp-position
-   #:free-font
+   #:ttf-font
 
    ;; sdl-util-ttf.lisp
    #:is-init
-;;   #:with-init
-   #:with-default-font
    #:with-open-font
    #:init-ttf
    #:quit-ttf
    #:initialise-font
    #:initialise-default-font
    #:close-font
-   #:get-Glyph-Metric
-   #:get-Font-Size
-   #:get-font-style
-   #:get-font-height
-   #:get-font-ascent
-   #:get-font-descent
-   #:get-font-line-skip
-   #:get-font-faces
-   #:is-font-face-fixed-width
-   #:get-font-face-family-name
-   #:get-font-face-style-name
    #:open-font
-   #:render-string-solid
-   #:draw-string-solid
-   #:draw-string-solid-*
-   #:render-string-shaded
-   #:draw-string-shaded
-   #:draw-string-shaded-*
-   #:render-string-blended
-   #:draw-string-blended
-   #:draw-string-blended-*
-   #:draw-font
-   #:draw-font-at
-   #:draw-font-at-*
-   #:set-font-style
-
-   ;;
-   
    ))
--- trunk/lispbuilder-sdl-ttf/sdl-ttf/string-solid.lisp
+++ trunk/lispbuilder-sdl-ttf/sdl-ttf/string-solid.lisp
@@ -2,88 +2,18 @@
 ;; 2006 (c) Rune Nesheim, see LICENCE.
 ;; 2007 (c) Luke Crook
 
-(in-package #:lispbuilder-sdl-ttf)
+(in-package #:lispbuilder-sdl)
 
-(defun render-string-solid (text &key
-			    (font *default-font*)
-			    (color sdl:*default-color*)
-			    (free nil)
-			    (cache nil))
-  "Render text `TEXT` using font `FONT` with color `COLOR` into a new `SURFACE`, using the Solid mode. 
-Unless `:FREE T`, the caller is responsible for freeing the new `SURFACE`.
-Use `:CACHE T` to cache the newly created surface in the `FONT` object.
-
-##### Parameters
-
-* `TEXT` is the text to render, of type `STRING`.
-* `FONT` is a `FONT` object. Binds to `\*DEFAULT-FONT\*` by default. 
-* `COLOR` is the color used to render the `TEXT`, of type `SDL:COLOR`
-* `FREE` when `T` will free the existing cached `SURFACE` in `FONT`.
-* `CACHE` when `T` will cache the newly created `SURFACE` in `FONT`.
-
-##### Returns
-
-* Returns the new surface `SDL:SDL-SURFACE`.
-
-##### Example
-
-     \(DRAW-STRING-SOLID \"Hello World!\" :FONT *DEFAULT-FONT* :COLOR A-COLOR\)"
-  (check-type font ttf-font)
-  (check-type color sdl:color)
-  (when free
-    (sdl:free-cached-surface font))
-  (let ((surf (sdl:with-foreign-color-copy (col-struct color)
-		(make-instance 'sdl:surface
-			       :fp (sdl-ttf-cffi::ttf-Render-UTF8-Solid (sdl:fp font) text col-struct)))))
-    (when cache
-      (setf (sdl:cached-surface font) surf))
-    surf))
-
-(defun draw-string-solid (text position &key 
-			  (font *default-font*)
-			  (surface sdl:*default-surface*)
-			  (color sdl:*default-color*))
-  "See [DRAW-STRING-SOLID-*](#draw-string-solid-*).
-
-##### Parmeters
-
-* `POSITION` is the `X` and `Y` coordinates to render `TEXT`, of type `SDL:POINT`."
-  (check-type position sdl:point) 
-  (draw-string-solid-* text (sdl:x position) (sdl:y position)
-		       :font font
-		       :surface surface
-		       :color color))
-
-(defun draw-string-solid-* (text x y &key 
-			    (font *default-font*)
-			    (surface sdl:*default-surface*)
-			    (color sdl:*default-color*))
-  "Draw text `TEXT` using font `FONT` with color `COLOR` onto surface `SURFACE`, using the Solid mode. 
-
-##### Parameters
-
-* `TEXT` is the text to render, of type `STRING`.
-* `X` and `Y` are the x and y coordinates of the `TEXT`, as `INTEGER`s.
-* `FONT` is a `FONT` object.  Binds to `\*DEFAULT-FONT\*` by default. 
-* `SURFACE` is the target surface, of type `SDL:SDL-SURFACE`. 
-* `COLOR` is the color used to render `TEXT`, of type `SDL:COLOR`
-
-##### Returns
-
-* Returns the target surface `SURFACE`.
-
-##### Example
-
-    \(DRAW-STRING-SOLID-* \"Hello World!\" 0 0 :FONT *DEFAULT-FONT* :SURFACE A-SURFACE :COLOR A-COLOR\)"
-  (unless surface
-    (setf surface sdl:*default-display*))
-  (check-type surface sdl:sdl-surface)
-  (sdl:with-surface (font-surface (render-string-solid text
-						       :font font
-						       :color color
-						       :free nil
-						       :cache nil)
-				  t)
+(defmethod _draw-string-solid-*_ ((string string) (x integer) (y integer) justify (surface sdl:sdl-surface) (font sdl-ttf:ttf-font) (color sdl:color))
+  (sdl:with-surface (font-surface (_render-string-solid_ string font color nil nil) t)
     (sdl:set-surface-* font-surface :x x :y y)
     (sdl:blit-surface font-surface surface))
   surface)
+
+(defmethod _render-string-solid_ ((string string) (font sdl-ttf:ttf-font) (color sdl:color) free cache)
+  (let ((surf (sdl:with-foreign-color-copy (col-struct color)
+		(make-instance 'sdl:surface
+			       :fp (sdl-ttf-cffi::ttf-Render-UTF8-Solid (sdl:fp font) string col-struct)))))
+    (when cache
+      (setf (sdl:cached-surface font) surf))
+    surf))
