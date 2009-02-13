02c22ef60baaaf352a0b7384eaca2907c35a7c72
Luke Crook <luke@balooga.com>
2009-02-13 18:38:50 +0000

    * Minor optimization to FPS code.

--- lispbuilder-sdl/examples/particles.lisp
+++ lispbuilder-sdl/examples/particles.lisp
@@ -140,7 +140,7 @@
       (unless (sdl:window *screen-width* *screen-height*
 			  :title-caption "Particles Demo"
 			  :icon-caption "Particles Demo"
-			  :flags sdl:sdl-hw-surface)
+                          :flags sdl:sdl-hw-surface)
 	(error "~&Unable to create a SDL window~%"))
 
       ;; Fix the framerate
@@ -210,11 +210,14 @@
 	 ;; Clear screen
 	 (sdl:clear-display sdl:*black*)
 
+         ;; Update the particles
+         (dolist (p *particles*)
+           (update-particle p *ftime*))
+         
          ;; Draw the particles
          (dolist (p *particles*)
-           (update-particle p *ftime*)
-	   (let ((pos (particle-pos p)))
-	     (sdl:draw-surface-at-* *particle-img*
+           (let ((pos (particle-pos p)))
+             (sdl:draw-surface-at-* *particle-img*
 				    (round (vec2-x pos)) (round (vec2-y pos)))))
 
 	 ;; Display text.
@@ -232,3 +235,107 @@
 
 	 ;; Update the timescale
 	 (setf *ftime* (sdl:time-scale)))))))
+
+(defun particles-unlocked ()
+  (let ((previous-average 0))
+
+    (sdl:with-init ()
+      ;; Create a window
+      (unless (sdl:window *screen-width* *screen-height*
+			  :title-caption "Particles Demo"
+			  :icon-caption "Particles Demo"
+                          :flags sdl:sdl-hw-surface
+                          :fps (make-instance 'sdl-base::fps-unlocked :dt 10))
+	(error "~&Unable to create a SDL window~%"))
+
+      ;; Fix the framerate
+      (setf (sdl:frame-rate) nil)
+      ;; Enable key repeat.
+      (sdl-cffi::sdl-enable-key-repeat sdl-cffi::sdl-default-repeat-delay
+				       sdl-cffi::sdl-default-repeat-interval)
+      ;; Load images. Convert the 24-bit particle surface into a 32bpp
+      ;; surface with an alpha component
+      (setf *particle-img* (sdl::convert-to-display-format :surface (sdl:load-image (sdl:create-path "particle.bmp" sdl:*default-asset-path*))
+                                                           :inherit nil
+							   :pixel-alpha t
+							   :free t))
+      
+      ;; Replace the alpha channel of *particle-img* with
+      ;; the alpha map in particle-alpha.bmp
+      (sdl:with-surface (alpha (sdl:load-image (sdl:create-path "particle-alpha.bmp"
+                                                                sdl:*default-asset-path*)))
+	(sdl:copy-channel-to-alpha *particle-img* alpha :channel :r))
+
+      ;; Load the bitmap fonts
+      (setf *font-large* (sdl:initialise-font sdl:*font-8x13*))
+      (setf *font-small* (sdl:initialise-font sdl:*font-7x14*))
+
+      ;; Render the initial text.
+      ;; We cache the rendered string in the FONT object.
+      ;; Drawing the font to the display then involves a fast blit.
+      (draw-cached-string (format nil "Particles Demo: {ESC} = Exit, {P} Add 100 Particles, {L} Remove 100 Particles.")
+                          5 5 *font-large* sdl:*default-display* t)
+      (draw-cached-string (format nil "Particles: ~d, Framerate: Calculating...."
+                                  *particle-count*)
+			  5 35 *font-small* sdl:*default-display* t)
+
+      ;; Create the particles
+      (setf *particles* nil)
+      (dotimes (i *particle-count*)
+	(push (init-particle (make-particle))
+	      *particles*))
+
+      (sdl:register-physics #'(lambda (ticks dt)
+                                (declare (ignore ticks))
+                                ;; Update the particles
+                                (dolist (p *particles*)
+                                  (update-particle p (/ dt 500)))))
+      ;; Event loop
+      (sdl:with-events ()
+	;; A key was pressed
+	(:key-down-event (:key k)
+         (cond
+          ;; Escape
+          ((sdl:key= k :sdl-key-escape)
+           (sdl:push-quit-event))
+          ;; Add particle
+          ((sdl:key= k :sdl-key-p)
+           (add-particles))
+          ;; Remove particle
+          ((sdl:key= k :sdl-key-l)
+           (remove-particles))))
+
+        (:mouse-motion-event (:x x :y y)
+         (setf *mouse-x* x
+               *mouse-y* y))
+
+        ;; Application is quitting
+	(:quit-event () t)
+
+	;; Redraw display
+	(:video-expose-event () (sdl:update-display))
+
+	;; Idle work
+	(:idle
+	 ;; Clear screen
+	 (sdl:clear-display sdl:*black*)
+         
+         ;; Draw the particles
+         (dolist (p *particles*)
+           (let ((pos (particle-pos p)))
+             (sdl:draw-surface-at-* *particle-img*
+				    (round (vec2-x pos)) (round (vec2-y pos)))))
+
+	 ;; Display text.
+         (draw-cached-string "" 5 5 *font-large* sdl:*default-display* nil)
+         (if (= (truncate (sdl:average-fps)) previous-average)
+           (draw-cached-string nil 5 35 *font-small* sdl:*default-display* nil)
+           (draw-cached-string (format nil "Particles: ~d, Framerate: ~f ftime: ~2f"
+                                       *particle-count*
+                                       (truncate (sdl:average-fps)) *ftime*)
+                               5 35 *font-small* sdl:*default-display* t))
+	 
+	 (setf previous-average (truncate (sdl:average-fps)))
+
+	 ;; Flip back/front buffers
+	 (sdl:update-display))))))
