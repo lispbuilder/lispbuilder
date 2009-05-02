c179869254fbcfaf38d79dacf3424a60d57ade7e
Luke Crook <luke@balooga.com>
2009-05-02 03:33:04 +0000

    * ARC, DOLLY and TRANSLATE are no longer tied to the TRACKBALL object.

--- lispbuilder-openrm/openrm/rm-aux.lisp
+++ lispbuilder-openrm/openrm/rm-aux.lisp
@@ -11,18 +11,6 @@
    (translate
     :accessor translate
     :initform (make-instance 'translate))
-   (scene
-    :reader scene
-    :initarg :scene)
-   (arc-node
-    :reader arc-node
-    :initarg :arc-node)
-   (target-node
-    :initarg :target)
-   (width
-    :initarg :width)
-   (height
-    :initarg :height)
    (enable-arc
     :accessor enable-arc-p
     :initarg :enable-arc)
@@ -33,7 +21,6 @@
     :accessor enable-translate-p
     :initarg :enable-translate))
   (:default-initargs
-   :arc-node (make-instance 'node)
    :enable-arc :button-middle
    :enable-dolly :button-right
    :enable-translate :button-left
@@ -43,37 +30,29 @@
   ((trackball
     :reader trackball)))
 
+(defclass aux-window (generic-window)())
+
+(defclass aux-scene (generic-scene)())
+
+(defmethod initialize-instance :after ((self trackball)
+                                       &key scene target dimensions &allow-other-keys)
+  (when scene
+    (setf (scene (dolly self)) scene
+          (scene (translate self)) scene
+          (scene (arc self)) scene))
+  (when target
+    (setf (target-node (arc self)) target))
+  (when dimensions
+    (setf (dimensions (dolly self)) dimensions
+          (dimensions (translate self)) dimensions
+          (dimensions (arc self)) dimensions)))
+
 (defmethod initialize-instance :after ((self aux-trackball)
                                        &key &allow-other-keys)
   (setf (slot-value self 'trackball)
         (make-instance 'trackball
                        :scene self
                        :target self)))
-
-(defclass aux-window (generic-window)())
-
-(defclass aux-scene (generic-scene)())
-
-(defmethod width ((self trackball))
-  (if (scene self)
-    (width (scene self))
-    (width self)))
-
-(defmethod height ((self trackball))
-  (if (scene self)
-    (height (scene self))
-    (height self)))
-
-(defmethod x-to-viewport ((self trackball) x)
-  (pixel-to-viewport x (width self)))
-
-(defmethod y-to-viewport ((self trackball) y)
-  (* -1.0 (pixel-to-viewport y (height self))))
-
-(defmethod target-node ((self trackball))
-  (if (slot-value self 'target-node)
-    (slot-value self 'target-node)
-    (scene self)))
 
 (defgeneric dolly-update (trackball y))
 (defgeneric dolly-reset (trackball y))
@@ -84,52 +63,71 @@
 
 (defmethod dolly-update ((self trackball) y)
   (update-dolly (dolly self)
-                (scene self)
-                (y-to-viewport self y)))
+                (y-to-viewport (dolly self) y)))
 
 (defmethod dolly-reset ((self trackball) y)
   (reset-dolly (dolly self)
-               (y-to-viewport self y)))
+               (y-to-viewport (dolly self) y)))
 
 (defmethod arc-update ((self trackball) x y)
   (update-arc (arc self)
-              (arc-node self)
-              (x-to-viewport self x)
-              (y-to-viewport self y)))
+              (x-to-viewport (arc self) x)
+              (y-to-viewport (arc self) y)))
 
 (defmethod arc-reset ((self trackball) x y)
   (reset-arc (arc self)
-             (arc-node self)
-             (x-to-viewport self x)
-             (y-to-viewport self y)))
+             (x-to-viewport (arc self) x)
+             (y-to-viewport (arc self) y)))
 
 (defmethod translate-update ((self trackball) x y)
   (update-translate (translate self)
-                    (scene self)
-                    (x-to-viewport self x)
-                    (y-to-viewport self y)))
+                    (x-to-viewport (translate self) x)
+                    (y-to-viewport (translate self) y)))
 
 (defmethod translate-reset ((self trackball) x y)
   (reset-translate (translate self)
-                   (x-to-viewport self x)
-                   (y-to-viewport self y)))
+                   (x-to-viewport (translate self) x)
+                   (y-to-viewport (translate self) y)))
 
-(defclass dolly ()
+(defclass arcball ()
+  ((scene-node :accessor scene :initarg :scene :initform nil)
+   (dimensions :accessor dimensions :initarg :dimensions :initform nil)))
+
+(defclass dolly (arcball)
   ((prev-button-y :accessor y-of :initform 0.0)))
 
-(defclass arc ()
+(defclass arc (arcball)
   ((prev-button-x :accessor x-of :initform 0.0)
    (prev-button-y :accessor y-of :initform 0.0)
    (initial-transform :accessor initial-transform-of :initform (rm-cffi::rm-matrix-new))
-   (result-transform :accessor result-transform-of :initform (rm-cffi::rm-matrix-new))))
+   (result-transform :accessor result-transform-of :initform (rm-cffi::rm-matrix-new))
+   (dummy-node :reader dummy-node :initform (make-instance 'node))
+   (target-node :accessor target-node :initarg :target :initform (make-instance 'node))))
 
-(defclass translate ()
+(defclass translate (arcball)
   ((prev-button-x :accessor x-of :initform 0.0)
    (prev-button-y :accessor y-of :initform 0.0)))
 
-(defgeneric update-dolly (dolly node y))
-(defmethod update-dolly ((dolly dolly) (node node) current-y)
-  (cffi:with-foreign-object (h-pointer :pointer)
+(defmethod width ((self arcball))
+  (if (scene self)
+    (width (scene self))
+    (width (dimensions self))))
+
+(defmethod height ((self arcball))
+  (if (scene self)
+    (height (scene self))
+    (height (dimensions self))))
+
+(defmethod x-to-viewport ((self arcball) x)
+  (pixel-to-viewport x (width self)))
+
+(defmethod y-to-viewport ((self arcball) y)
+  (* -1.0 (pixel-to-viewport y (height self))))
+
+(defgeneric update-dolly (dolly y))
+(defmethod update-dolly ((dolly dolly) current-y)
+  (let ((node (scene dolly)))
+    (cffi:with-foreign-object (h-pointer :pointer)
     (when (rm-cffi::rm-node-get-scene-camera-3d (fp node) h-pointer)
       (let ((camera-pointer (cffi:mem-aref h-pointer :pointer)))
         (cffi:with-foreign-objects ((x :float) (y :float) (prev-x :float) (prev-y :float))
@@ -140,15 +138,16 @@
           (rm-cffi::rmaux-dolly camera-pointer x y prev-x prev-y))
         (rm-cffi::rm-Node-Set-Scene-Camera-3D (fp node) camera-pointer)
         (rm-cffi::rm-Camera-3D-Delete camera-pointer)
-        (setf (y-of dolly) current-y)))))
+        (setf (y-of dolly) current-y))))))
 
 (defgeneric reset-dolly (dolly y))
 (defmethod reset-dolly ((dolly dolly) y)
   (setf (y-of dolly) y))
 
-(defgeneric update-arc (arc node current-x current-y))
-(defmethod update-arc ((arc arc) (node node) current-x current-y)
-  (let ((initial-transform (initial-transform-of arc))
+(defgeneric update-arc (arc current-x current-y))
+(defmethod update-arc ((arc arc) current-x current-y)
+  (let ((node (dummy-node arc))
+        (initial-transform (initial-transform-of arc))
 	(result-transform (result-transform-of arc)))
     (cffi:with-foreign-objects ((x :float) (y :float) (prev-x :float) (prev-y :float))
       (setf (cffi:mem-aref x :float) (x-of arc)
@@ -159,28 +158,30 @@
     (rm-cffi::rm-Matrix-Multiply initial-transform result-transform result-transform)
     (rm-cffi::rm-Node-Set-Rotate-Matrix (fp node) result-transform)))
 
-(defgeneric reset-arc (arc node x y))
-(defmethod reset-arc ((arc arc) (node node) x y)
+(defgeneric reset-arc (arc x y))
+(defmethod reset-arc ((arc arc) x y)
   (setf (x-of arc) x
 	(y-of arc) y)
-  (when (eq (rm-cffi::rm-Node-Get-Rotate-Matrix (fp node) (initial-transform-of arc)) :rm-false)
+  (when (eq (rm-cffi::rm-Node-Get-Rotate-Matrix (fp (dummy-node arc))
+                                                (initial-transform-of arc)) :rm-false)
     (rm-cffi::rm-Matrix-Identity (initial-transform-of arc))))
 
-(defgeneric update-translate (translate node current-x current-y))
-(defmethod update-translate ((translate translate) (node node) current-x current-y)
-  (cffi:with-foreign-object (h-pointer :pointer)
-    (when (rm-cffi::rm-node-get-scene-camera-3d (fp node) h-pointer)
-      (let ((camera-pointer (cffi:mem-aref h-pointer :pointer)))
-        (cffi:with-foreign-objects ((x :float) (y :float) (prev-x :float) (prev-y :float))
-          (setf (cffi:mem-aref x :float) (x-of translate)
-                (cffi:mem-aref y :float) (y-of translate)
-                (cffi:mem-aref prev-x :float) current-x
-                (cffi:mem-aref prev-y :float) current-y)		      
-          (rm-cffi::rmaux-translate camera-pointer x y prev-x prev-y))
-        (rm-cffi::rm-Node-Set-Scene-Camera-3D (fp node) camera-pointer)
-        (rm-cffi::rm-Camera-3D-Delete camera-pointer)
-        (setf (x-of translate) current-x
-              (y-of translate) current-y)))))
+(defgeneric update-translate (translate current-x current-y))
+(defmethod update-translate ((translate translate) current-x current-y)
+  (let ((node (scene translate)))
+    (cffi:with-foreign-object (h-pointer :pointer)
+      (when (rm-cffi::rm-node-get-scene-camera-3d (fp node) h-pointer)
+        (let ((camera-pointer (cffi:mem-aref h-pointer :pointer)))
+          (cffi:with-foreign-objects ((x :float) (y :float) (prev-x :float) (prev-y :float))
+            (setf (cffi:mem-aref x :float) (x-of translate)
+                  (cffi:mem-aref y :float) (y-of translate)
+                  (cffi:mem-aref prev-x :float) current-x
+                  (cffi:mem-aref prev-y :float) current-y)		      
+            (rm-cffi::rmaux-translate camera-pointer x y prev-x prev-y))
+          (rm-cffi::rm-Node-Set-Scene-Camera-3D (fp node) camera-pointer)
+          (rm-cffi::rm-Camera-3D-Delete camera-pointer)
+          (setf (x-of translate) current-x
+                (y-of translate) current-y))))))
 
 (defgeneric reset-translate (translate x y))
 (defmethod reset-translate ((translate translate) x y)
@@ -200,7 +201,7 @@
     (dolly-update self y))
   (when (button= button (enable-arc-p self)) 
     (arc-update self x y)
-    (rotate (target-node self) (arc-node self)))
+    (rotate (target-node (arc self)) (dummy-node (arc self))))
   (when (button= button (enable-translate-p self)) 
     (translate-update self x y)))
 
