f7311563c89a2be0ca35d625a6f36cd9712075d7
Luke Crook <luke@balooga.com>
2009-09-11 01:56:23 +0000

    * Fixed to support CCL on win32.

--- lispbuilder-sdl/cffi/syswm.lisp
+++ lispbuilder-sdl/cffi/syswm.lisp
@@ -10,45 +10,45 @@
 (cffi:defcstruct HDC__
 	(unused :int))
 
-#+win32 (defcstruct SDL-Sys-WM-msg
-	(version SDL-version)
-	(hwnd :pointer)
-	(msg :pointer)
-	(wParam :unsigned-int)
-	(lParam :long))
+#+windows(defcstruct SDL-Sys-WM-msg
+           (version SDL-version)
+           (hwnd :pointer)
+           (msg :pointer)
+           (wParam :unsigned-int)
+           (lParam :long))
 
-#+win32 (defcstruct SDL-Sys-WM-info
-	(version SDL-version)
-	(window :pointer)
-	(hglrc :pointer))
+#+windows(defcstruct SDL-Sys-WM-info
+           (version SDL-version)
+           (window :pointer)
+           (hglrc :pointer))
 
-#-win32 (defcenum SDL-SYS-WM-TYPE
-	:SDL-SYS-WM-X11)
+#-windows(defcenum SDL-SYS-WM-TYPE
+           :SDL-SYS-WM-X11)
 
-#-win32 (defcunion SDL-Sys-WM-msg-event
-	(xevent :pointer))
+#-windows(defcunion SDL-Sys-WM-msg-event
+           (xevent :pointer))
 
-#-win32 (defcstruct SDL-Sys-WM-msg
-	(version SDL-version)
-	(subsystem SDL-SYS-WM-TYPE)
-	(event SDL-Sys-WM-msg-event))
+#-windows(defcstruct SDL-Sys-WM-msg
+           (version SDL-version)
+           (subsystem SDL-SYS-WM-TYPE)
+           (event SDL-Sys-WM-msg-event))
 
-#-win32 (defcstruct SDL-Sys-WM-info-info-x11
-	(display :pointer)
-	(window :unsigned-long)
-	(lock-func :pointer)
-	(unlock-func :pointer)
-	(fswindow :unsigned-long)
-	(wmwindow :unsigned-long)
-	(gfxdisplay :pointer))
+#-windows(defcstruct SDL-Sys-WM-info-info-x11
+           (display :pointer)
+           (window :unsigned-long)
+           (lock-func :pointer)
+           (unlock-func :pointer)
+           (fswindow :unsigned-long)
+           (wmwindow :unsigned-long)
+           (gfxdisplay :pointer))
 
-#-win32 (defcunion SDL-Sys-WM-info-info
-	(x11 SDL-Sys-WM-info-info-x11))
+#-windows(defcunion SDL-Sys-WM-info-info
+           (x11 SDL-Sys-WM-info-info-x11))
 
-#-win32 (defcstruct SDL-Sys-WM-info
-	(version SDL-version)
-	(subsystem SDL-SYS-WM-TYPE)
-	(info SDL-Sys-WM-info-info))
+#-windows(defcstruct SDL-Sys-WM-info
+           (version SDL-version)
+           (subsystem SDL-SYS-WM-TYPE)
+           (info SDL-Sys-WM-info-info))
 
 (defcfun ("SDL_GetWMInfo" SDL-Get-WM-Info) :int
   (info :pointer))
