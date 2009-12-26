be4ecf4aea46f983f18d04dff0bc639b133d2aca
Luke Crook <luke@balooga.com>
2009-12-26 08:55:28 +0000

    * Fixes for SBCL/win32

--- lispbuilder-sdl/cffi/glue.lisp
+++ lispbuilder-sdl/cffi/glue.lisp
@@ -1,22 +1,39 @@
 
 (in-package #:lispbuilder-sdl-cffi)
 
-(cffi:defcfun ("SDL_glue_SDL_OpenAudio"
-               sdl-glue-SDL-Open-Audio) :int
-  (desired :pointer)
-  (obtained :pointer))
+#+(and sbcl lispbuilder-sdl-audio (or mswindows windows win32))
+(progn
+  (cffi:defcfun ("SDL_glue_SDL_OpenAudio"
+		 sdl-glue-SDL-Open-Audio) :int
+    (desired :pointer)
+    (obtained :pointer))
+	
+  (cffi:defcfun ("SDL_glue_SDL_CloseAudio"
+		 SDL-glue-SDL-Close-Audio) :void)
+	
+  (cffi:defcfun ("SDL_glue_SDL_BufferFilled"
+		 SDL-glue-SDL-Buffer-Filled) :int)
 
-(cffi:defcfun ("SDL_glue_SDL_CloseAudio"
-               SDL-glue-SDL-Close-Audio) :void)
+  (cffi:defcfun ("SDL_glue_SDL_RequireBufferFill"
+		 SDL-glue-SDL-Require-Buffer-Fill) :int)
 
-(cffi:defcfun ("SDL_glue_SDL_BufferFilled"
-               SDL-glue-SDL-Buffer-Filled) :int)
+  (cffi:defcfun ("SDL_glue_SDL_GetAudioBuffer"
+		 SDL-glue-SDL-Get-Audio-Buffer) :pointer)
 
-(cffi:defcfun ("SDL_glue_SDL_RequireBufferFill"
-               SDL-glue-SDL-Require-Buffer-Fill) :int)
+  (cffi:defcfun ("SDL_glue_SDL_GetAudioBufferLength"
+		 SDL-glue-SDL-Get-Audio-Buffer-length) :int))
+#-sbcl(progn
+	(cffi:defcfun ("SDL_glue_SDL_CloseAudio"
+		       SDL-glue-SDL-Close-Audio) :void)
+	
+	(cffi:defcfun ("SDL_glue_SDL_BufferFilled"
+		       SDL-glue-SDL-Buffer-Filled) :int)
 
-(cffi:defcfun ("SDL_glue_SDL_GetAudioBuffer"
-               SDL-glue-SDL-Get-Audio-Buffer) :pointer)
+	(cffi:defcfun ("SDL_glue_SDL_RequireBufferFill"
+		       SDL-glue-SDL-Require-Buffer-Fill) :int)
 
-(cffi:defcfun ("SDL_glue_SDL_GetAudioBufferLength"
-               SDL-glue-SDL-Get-Audio-Buffer-length) :int)
+	(cffi:defcfun ("SDL_glue_SDL_GetAudioBuffer"
+		       SDL-glue-SDL-Get-Audio-Buffer) :pointer)
+
+	(cffi:defcfun ("SDL_glue_SDL_GetAudioBufferLength"
+		       SDL-glue-SDL-Get-Audio-Buffer-length) :int))
