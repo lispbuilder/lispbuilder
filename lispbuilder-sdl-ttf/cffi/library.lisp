4eb150113443e49b2f9f5e6f1e4f679a853e5b36
Luke Crook <luke@balooga.com>
2009-12-29 08:34:52 +0000

    * Updated Makefiles.

--- lispbuilder-sdl-ttf/cffi/library.lisp
+++ lispbuilder-sdl-ttf/cffi/library.lisp
@@ -7,10 +7,10 @@
 ;;	   cffi:*foreign-library-directories*
 ;;	   :test #'equal))
 
-#+windows(eval-when (:compile-toplevel :load-toplevel :execute)
-           (pushnew sdl-ttf-bin:*dll-path*
-                    cffi:*foreign-library-directories*
-                    :test #'equal))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (pushnew sdl-ttf-bin:*dll-path*
+           cffi:*foreign-library-directories*
+           :test #'equal))
 
 (cffi:define-foreign-library zlib1
   (:windows "zlib1.dll")
@@ -24,15 +24,15 @@
 
 (cffi:define-foreign-library sdl-ttf
   (:darwin (:or (:framework "SDL_ttf")
-		(:framework "libSDL_ttf-2.0")))
+            (:framework "libSDL_ttf-2.0")))
   (:windows (:or "SDL_ttf.dll"))
   (:unix (:or "libSDL_ttf" "libSDL_ttf2.0" "libSDL_ttf-2.0.so.0")))
 
 (cffi:define-foreign-library sdl-ttf-glue
-  (:darwin (:or "liblispbuilder-sdl-ttf-glue.dylib"
-		"lispbuilder-sdl-ttf-glue.dylib"))
+  (:darwin (:or "liblispbuilder-sdl-ttf-glue"
+            "lispbuilder-sdl-ttf-glue"))
   (:windows (:or "liblispbuilder-sdl-ttf-glue.dll"
-		 "lispbuilder-sdl-ttf-glue.dll"))
+             "lispbuilder-sdl-ttf-glue.dll"))
   (:unix "liblispbuilder-sdl-ttf-glue.so"))
 
 ;; Load the win32 required libraries.
@@ -41,8 +41,13 @@
 
 ;; Load the general sdl libraries.
 (cffi:use-foreign-library sdl-ttf)
-;;(cffi:use-foreign-library sdl-ttf-glue)
+
+;; cffi:foreign-library-loaded-p is not yet in the released version of CFFI
+;;(defparameter *glue-loaded-p* (cffi:foreign-library-loaded-p 'sdl-glue))
+(defparameter *glue-loaded-p* nil)
 
 (when (handler-case (cffi:use-foreign-library sdl-ttf-glue)
         (load-foreign-library-error () nil))
-  (pushnew :lispbuilder-sdl-ttf-glue *features*))
+  (progn
+    (setf *glue-loaded-p* t)
+    (pushnew :lispbuilder-sdl-ttf-glue *features*)))
