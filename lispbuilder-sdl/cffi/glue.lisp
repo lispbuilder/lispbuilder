7cb599337c9c0fcd710ae9e620b4230d4c72f9ae
Luke Crook <luke@balooga.com>
2010-01-03 20:16:57 +0000

    * Fixed glue bug to support Linux

--- lispbuilder-sdl/cffi/glue.lisp
+++ lispbuilder-sdl/cffi/glue.lisp
@@ -1,7 +1,7 @@
 
 (in-package #:lispbuilder-sdl-cffi)
 
-#+(and sbcl lispbuilder-sdl-audio (or mswindows windows win32))
+#+lispbuilder-sdl-audio
 (progn
   (cffi:defcfun ("SDL_glue_SDL_OpenAudio"
 		 sdl-glue-SDL-Open-Audio) :int
@@ -22,22 +22,4 @@
 
   (cffi:defcfun ("SDL_glue_SDL_GetAudioBufferLength"
 		 SDL-glue-SDL-Get-Audio-Buffer-length) :int))
-#-sbcl(progn
-	(cffi:defcfun ("SDL_glue_SDL_OpenAudio"
-		       sdl-glue-SDL-Open-Audio) :int
-	  (desired :pointer)
-	  (obtained :pointer))
-	(cffi:defcfun ("SDL_glue_SDL_CloseAudio"
-		       SDL-glue-SDL-Close-Audio) :void)
-	
-	(cffi:defcfun ("SDL_glue_SDL_BufferFilled"
-		       SDL-glue-SDL-Buffer-Filled) :int)
 
-	(cffi:defcfun ("SDL_glue_SDL_RequireBufferFill"
-		       SDL-glue-SDL-Require-Buffer-Fill) :int)
-
-	(cffi:defcfun ("SDL_glue_SDL_GetAudioBuffer"
-		       SDL-glue-SDL-Get-Audio-Buffer) :pointer)
-
-	(cffi:defcfun ("SDL_glue_SDL_GetAudioBufferLength"
-		       SDL-glue-SDL-Get-Audio-Buffer-length) :int))
