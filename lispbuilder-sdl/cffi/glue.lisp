f322b49399e9f18dbb38771161d69b6d5c3f06eb
Luke Crook <luke@balooga.com>
2010-01-11 09:01:26 +0000

    * Removed old glue definitions.

--- lispbuilder-sdl/cffi/glue.lisp
+++ lispbuilder-sdl/cffi/glue.lisp
@@ -1,28 +1,6 @@
 
 (in-package #:lispbuilder-sdl-cffi)
 
-#+lispbuilder-sdl-audio
-(progn
-  (cffi:defcfun ("SDL_glue_SDL_OpenAudio"
-		 sdl-glue-SDL-Open-Audio) :int
-    (desired :pointer)
-    (obtained :pointer))
-	
-  (cffi:defcfun ("SDL_glue_SDL_CloseAudio"
-		 SDL-glue-SDL-Close-Audio) :void)
-	
-  (cffi:defcfun ("SDL_glue_SDL_BufferFilled"
-		 SDL-glue-SDL-Buffer-Filled) :int)
-
-  (cffi:defcfun ("SDL_glue_SDL_RequireBufferFill"
-		 SDL-glue-SDL-Require-Buffer-Fill) :int)
-
-  (cffi:defcfun ("SDL_glue_SDL_GetAudioBuffer"
-		 SDL-glue-SDL-Get-Audio-Buffer) :pointer)
-
-  (cffi:defcfun ("SDL_glue_SDL_GetAudioBufferLength"
-		 SDL-glue-SDL-Get-Audio-Buffer-length) :int))
-
 ;; SBCL and CLISP should never, ever use the raw SDL audio routines without the glue library,
 ;; or nastly crashes will result.
 (defun open-audio (desired-spec obtained-spec)
