150f276b0d2fbf3ee6cf091f4f1b438cfdae9eaa
luke@balooga.com <luke@balooga.com@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2010-04-25 15:35:46 +0000

    * Fix for Lispworks 6/Windows 7 64-bit

--- trunk/lispbuilder-sdl/cffi/glue.lisp
+++ trunk/lispbuilder-sdl/cffi/glue.lisp
@@ -7,7 +7,7 @@
   (if (cffi:foreign-symbol-pointer "SDL_glue_SDL_OpenAudio")
       (cffi:foreign-funcall-pointer (cffi:foreign-symbol-pointer "SDL_glue_SDL_OpenAudio") () :pointer desired-spec :pointer obtained-spec :int)
       #+(or clisp sbcl)(progn -1)
-      #-(or clisp sbcl)(cffi:foreign-funcall-pointer (cffi:foreign-symbol-pointer "SDL_OpenAudio" :library 'sdl) () :pointer desired-spec :pointer obtained-spec :int)))
+      #-(or clisp sbcl)(cffi:foreign-funcall-pointer (cffi:foreign-symbol-pointer "SDL_OpenAudio") () :pointer desired-spec :pointer obtained-spec :int)))
 
 ;; SBCL and CLISP should never, ever use the raw SDL audio routines without the glue library,
 ;; or nastly crashes will result.
@@ -15,7 +15,7 @@
   (if (cffi:foreign-symbol-pointer "SDL_glue_SDL_CloseAudio")
       (cffi:foreign-funcall-pointer (cffi:foreign-symbol-pointer "SDL_glue_SDL_CloseAudio") () :void)
       #+(or clisp sbcl cmucl)(progn -1)
-      #-(or clisp sbcl cmucl)(cffi:foreign-funcall-pointer (cffi:foreign-symbol-pointer "SDL_CloseAudio" :library 'sdl) () :void)))
+      #-(or clisp sbcl cmucl)(cffi:foreign-funcall-pointer (cffi:foreign-symbol-pointer "SDL_CloseAudio") () :void)))
 
 (defun require-buffer-fill ()
   (let ((fp (cffi:foreign-symbol-pointer "SDL_glue_SDL_RequireBufferFill")))
