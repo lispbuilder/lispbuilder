ed5b024ab0929a9961f238204daafd858c50ee8b
luke@balooga.com <luke@balooga.com@ad8a5436-b7f1-11dd-b55b-2da8d274f894>
2010-04-25 15:36:10 +0000

    * Fix for Lispworks 6

--- trunk/lispbuilder-sdl-mixer/sdl-mixer/mixer.lisp
+++ trunk/lispbuilder-sdl-mixer/sdl-mixer/mixer.lisp
@@ -3,7 +3,7 @@
 
 ;; Configure Lispworks to allow
 ;; callbacks from foreign threads
-#+(and lispworks (not lispworks5.1) (not lispworks4.3)) (system:setup-for-alien-threads)
+#+(and lispworks (not lispworks5.1) (not lispworks4.3) (not lispworks6.0)) (system:setup-for-alien-threads)
 
 ;;;; Add documentation strings for defconstants in cffi/sdl-mixer.lisp
 (setf (documentation '+default-format+ 'variable)
